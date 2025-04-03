cf86a6201fb8e967e43a4fd9fe4246d2e965925e
33hyun <lggx12345@gmail.com>
2025-04-03 19:45:24 +0900

    feat: 닉네임 폼 생성 및 소셜 로그인 성공(Authentication에서 로그인 데이터 확인)

---
+++ src/components/auth/NicknameForm.tsx
@@ -0,0 +1,42 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { z } from "zod";
+import { supabase } from "@/supabase/client";
+import { Button } from "../ui/button";
+import { Input } from "../ui/input";
+
+const schema = z.object({
+  nickname: z.string().min(2, "닉네임은 두 글자 이상!"),
+});
+
+type FormData = z.infer<typeof schema>;
+
+const NicknameForm = ({ userId }: { userId: string }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: {},
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  const onSubmit = async (data: FormData) => {
+    await supabase.from("users").insert({
+      id: userId,
+      nickname: data.nickname,
+      profile_image: "",
+    });
+    location.href = "/"; // 홈으로 리다이렉트
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <Input placeholder="닉네임 입력" {...register("nickname")} />
+      <Button type="submit">등록</Button>
+    </form>
+  );
+};
+
+export default NicknameForm;
