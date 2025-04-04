17d43e5b795787bc6b62ad94b7412cba6d5bd699
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-04-04 21:56:25 +0900

    feat: 지도 API 기능 연결 모바일 기준으로 화면 띄우기

--- src/app/(pages)/map/page.tsx
+++ src/app/(pages)/map/page.tsx
@@ -1,7 +1,49 @@
-import React from "react";
+"use client";
+
+import { Input } from "@/components/ui/input";
+import { useMapStore } from "@/store/useMapStore";
+import { Search } from "lucide-react";
+import { useRef } from "react";
+import { Map, MapMarker } from "react-kakao-maps-sdk";
 
 const MapPage = () => {
-  return <div>map page</div>;
+  const mapRef = useRef<kakao.maps.Map | null>(null); // 지도 참조 생성
+  const { center, setCenter } = useMapStore();
+
+  return (
+    <div className="flex justify-center flex-col max-w-[393px] mx-auto h-[852px] items-center ">
+      <div className="flex flex-row justify-center relative">
+        <Input
+          type="text"
+          className="w-[343px] h-[48px] bg-white absolute top-5 z-10 p-3"
+          placeholder="입력해주세요"
+        />
+        <button className="cursor-pointer">
+          <Search className="absolute top-8 z-[999] left-[140px] active:text-blue-500" />
+        </button>
+      </div>
+      <Map
+        id="map"
+        center={center}
+        level={3} // 지도의 확대 레벨
+        onCreate={(map) => (mapRef.current = map)} // 지도 객체 저장
+        className="w-full h-full"
+      >
+        <MapMarker
+          position={center}
+          onClick={() => {
+            if (mapRef.current) {
+              // 마커 클릭 시 지도 중심을 이동시킴
+              mapRef.current.panTo(
+                new kakao.maps.LatLng(center.lat, center.lng)
+              );
+            }
+            setCenter({ lat: center.lat, lng: center.lng });
+          }}
+        ></MapMarker>
+      </Map>
+    </div>
+  );
 };
 
 export default MapPage;
