2e5d0ae3fd765bdfc96e2376bdd113b16bc76428
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-04 12:59:18 +0900

    init: 페이지 폴더와 각 페이지 폴더/파일 추가

---
+++ src/app/(pages)/Error.tsx
@@ -0,0 +1,5 @@
+function Error() {
+  return <p>에러</p>;
+}
+
+export default Error;
---
+++ src/app/(pages)/map/[id]/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ShelterDetailPage = () => {
+  return <div>page</div>;
+};
+
+export default ShelterDetailPage;
