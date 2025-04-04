8456f8cfc127fd7028e18e1e6a15cca5ec2d7fe5
nahyun park <pnh13584@gmail.com>
2025-04-04 21:16:35 +0900

    fix: export 오류 수정

--- src/components/ProfileTabs.tsx
+++ src/components/ProfileTabs.tsx
@@ -1,18 +1,24 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/Tabs";
 
-<Tabs defaultValue="post" className="w-[400px]">
-  <TabsList>
-    {/* 탭 메뉴 */}
-    <TabsTrigger value="post">작성 게시글</TabsTrigger>
-    <TabsTrigger value="comment">참여 게시글</TabsTrigger>
-    <TabsTrigger value="useful">저장된 게시글</TabsTrigger>
-  </TabsList>
-  {/* 트리거에 맞는 내용 */}
-  <TabsContent value="post">여기에 작성한 게시글이 표시됩니다.</TabsContent>
-  <TabsContent value="comment">
-    여기에 댓글을 단 게시글이 표시됩니다.
-  </TabsContent>
-  <TabsContent value="useful">
-    여기에 유익해요한 게시글이 표시됩니다.
-  </TabsContent>
-</Tabs>;
+const ProfileTabs = () => {
+  return (
+    <Tabs defaultValue="post" className="w-[400px]">
+      <TabsList>
+        {/* 탭 메뉴 */}
+        <TabsTrigger value="post">작성 게시글</TabsTrigger>
+        <TabsTrigger value="comment">참여 게시글</TabsTrigger>
+        <TabsTrigger value="useful">저장된 게시글</TabsTrigger>
+      </TabsList>
+      {/* 트리거에 맞는 내용 */}
+      <TabsContent value="post">여기에 작성한 게시글이 표시됩니다.</TabsContent>
+      <TabsContent value="comment">
+        여기에 댓글을 단 게시글이 표시됩니다.
+      </TabsContent>
+      <TabsContent value="useful">
+        여기에 유익해요한 게시글이 표시됩니다.
+      </TabsContent>
+    </Tabs>
+  );
+};
+
+export default ProfileTabs;
