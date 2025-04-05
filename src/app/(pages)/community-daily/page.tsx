1ae5321909c78a2a87bc8ae05ad5d25d50ab1087
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-06 06:55:12 +0900

    style: carousel를 이용한 이미지 넘김 포함 게시글 디자인 작업

--- src/app/(pages)/community-daily/page.tsx
+++ src/app/(pages)/community-daily/page.tsx
@@ -9,46 +9,82 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+
 import { ThumbsUp } from "lucide-react";
 
 const CommunityDailyPage = () => {
   const USEFULNUM = 10;
+  const DUMMY_TEXT =
+    "지방자치단체는 법령의 범위안에서 자치에 관한 규정을 제정할 수 있다.";
 
   return (
     <main className="min-w-screen h-screen flex flex-col justify-center items-center">
-      <Card className="pt-3">
-        <CardHeader className="w-[300px] h-[400px] p-3">
+      <Card className="w-[350px] p-5 pb-10 gap-3">
+        <CardHeader className="p-0">
           {/* 작성자 아바타 이미지 & 닉네임*/}
-          <section className="flex flex-row items-center gap-2 pb-2">
+          <section className="flex flex-row items-center gap-2">
             <Avatar>
               <AvatarImage src="https://github.com/shadcn.png" />
               <AvatarFallback>CN</AvatarFallback>
             </Avatar>
             <span>Avatar Name</span>
           </section>
-
-          <CardDescription>
-            <Image
-              src={sampleImage}
-              alt="sampleImage"
-              width={300}
-              height={300}
-              className="w-[280px] h-[350px]"
-            />
-
-            {/* "유용해요 아이콘 & 개수" */}
-            <section className="flex flex-row justify-between items-row">
-              <span>유용해요 {USEFULNUM}개</span>
-              <ThumbsUp></ThumbsUp>
-            </section>
-          </CardDescription>
         </CardHeader>
-        <CardContent>
-          <p>Card Content</p>
+        <CardContent className="p-0">
+          <Carousel className="relative border-2 border-black">
+            <CarouselContent className=" z-0">
+              <CarouselItem>
+                <Image
+                  src={sampleImage}
+                  alt="sampleImage"
+                  width={330}
+                  height={350}
+                  className="w-[330px] h-[350px]"
+                />
+              </CarouselItem>
+              <CarouselItem>
+                {" "}
+                <Image
+                  src={sampleImage}
+                  alt="sampleImage"
+                  width={330}
+                  height={350}
+                  className="w-[330px] h-[350px]"
+                />
+              </CarouselItem>
+              <CarouselItem>
+                {" "}
+                <Image
+                  src={sampleImage}
+                  alt="sampleImage"
+                  width={330}
+                  height={350}
+                  className="w-[330px] h-[350px]"
+                />
+              </CarouselItem>
+            </CarouselContent>
+            <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10" />
+            <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10" />
+          </Carousel>
+
+          {/* "유용해요 아이콘 & 개수" */}
+          <section className="flex flex-row justify-between items-row">
+            <span className="text-gray-500">유용해요 {USEFULNUM}개</span>
+            <ThumbsUp></ThumbsUp>
+          </section>
+          <section className="flex flex-col p-0 mt-2 mb-4 gap-2">
+            <CardTitle>게시글 제목</CardTitle>
+            <p className="text-sm">{DUMMY_TEXT}</p>
+          </section>
+          <p className="text-gray-500">댓글 컴포넌트 들어갈 공간</p>
         </CardContent>
-        <CardFooter>
-          <p>Card Footer</p>
-        </CardFooter>
       </Card>
     </main>
   );
