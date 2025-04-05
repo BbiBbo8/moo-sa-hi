3971c1be6f6aad91f91c5273147a7b246c78c2cd
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-06 07:04:45 +0900

    style: tab추가 완료

--- src/app/(pages)/community-daily/page.tsx
+++ src/app/(pages)/community-daily/page.tsx
@@ -1,13 +1,6 @@
 import Image from "next/image";
 import sampleImage from "../../../../public/sampleScreenshot.png";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import {
   Carousel,
@@ -16,7 +9,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { ThumbsUp } from "lucide-react";
 
 const CommunityDailyPage = () => {
@@ -25,67 +18,81 @@ const CommunityDailyPage = () => {
     "지방자치단체는 법령의 범위안에서 자치에 관한 규정을 제정할 수 있다.";
 
   return (
-    <main className="min-w-screen h-screen flex flex-col justify-center items-center">
-      <Card className="w-[350px] p-5 pb-10 gap-3">
-        <CardHeader className="p-0">
-          {/* 작성자 아바타 이미지 & 닉네임*/}
-          <section className="flex flex-row items-center gap-2">
-            <Avatar>
-              <AvatarImage src="https://github.com/shadcn.png" />
-              <AvatarFallback>CN</AvatarFallback>
-            </Avatar>
-            <span>Avatar Name</span>
-          </section>
-        </CardHeader>
-        <CardContent className="p-0">
-          <Carousel className="relative border-2 border-black">
-            <CarouselContent className=" z-0">
-              <CarouselItem>
-                <Image
-                  src={sampleImage}
-                  alt="sampleImage"
-                  width={330}
-                  height={350}
-                  className="w-[330px] h-[350px]"
-                />
-              </CarouselItem>
-              <CarouselItem>
-                {" "}
-                <Image
-                  src={sampleImage}
-                  alt="sampleImage"
-                  width={330}
-                  height={350}
-                  className="w-[330px] h-[350px]"
-                />
-              </CarouselItem>
-              <CarouselItem>
-                {" "}
-                <Image
-                  src={sampleImage}
-                  alt="sampleImage"
-                  width={330}
-                  height={350}
-                  className="w-[330px] h-[350px]"
-                />
-              </CarouselItem>
-            </CarouselContent>
-            <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10" />
-            <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10" />
-          </Carousel>
+    <main className="relative min-w-screen h-screen flex flex-col justify-center items-center">
+      <Tabs
+        defaultValue="account"
+        className="w-[400px] flex flex-col justify-center items-center"
+      >
+        <TabsList className="absolute top-16">
+          <TabsTrigger value="account">Account</TabsTrigger>
+          <TabsTrigger value="password">Password</TabsTrigger>
+        </TabsList>
+        <TabsContent value="account">대피소 커뮤니티 페이지</TabsContent>
+        <TabsContent value="password">
+          <section className="flex flex-col justify-center ">
+            <Card className="w-[350px] p-5 pb-10 gap-3">
+              <CardHeader className="p-0">
+                {/* 작성자 아바타 이미지 & 닉네임*/}
+                <section className="flex flex-row items-center gap-2">
+                  <Avatar>
+                    <AvatarImage src="https://github.com/shadcn.png" />
+                    <AvatarFallback>CN</AvatarFallback>
+                  </Avatar>
+                  <span>Avatar Name</span>
+                </section>
+              </CardHeader>
+              <CardContent className="p-0">
+                <Carousel className="relative border-2 border-black">
+                  <CarouselContent className=" z-0">
+                    <CarouselItem>
+                      <Image
+                        src={sampleImage}
+                        alt="sampleImage"
+                        width={330}
+                        height={350}
+                        className="w-[330px] h-[350px]"
+                      />
+                    </CarouselItem>
+                    <CarouselItem>
+                      {" "}
+                      <Image
+                        src={sampleImage}
+                        alt="sampleImage"
+                        width={330}
+                        height={350}
+                        className="w-[330px] h-[350px]"
+                      />
+                    </CarouselItem>
+                    <CarouselItem>
+                      {" "}
+                      <Image
+                        src={sampleImage}
+                        alt="sampleImage"
+                        width={330}
+                        height={350}
+                        className="w-[330px] h-[350px]"
+                      />
+                    </CarouselItem>
+                  </CarouselContent>
+                  <CarouselPrevious className="absolute left-2 top-1/2 -translate-y-1/2 z-10" />
+                  <CarouselNext className="absolute right-2 top-1/2 -translate-y-1/2 z-10" />
+                </Carousel>
 
-          {/* "유용해요 아이콘 & 개수" */}
-          <section className="flex flex-row justify-between items-row">
-            <span className="text-gray-500">유용해요 {USEFULNUM}개</span>
-            <ThumbsUp></ThumbsUp>
+                {/* "유용해요 아이콘 & 개수" */}
+                <section className="flex flex-row justify-between items-row">
+                  <span className="text-gray-500">유용해요 {USEFULNUM}개</span>
+                  <ThumbsUp></ThumbsUp>
+                </section>
+                <section className="flex flex-col p-0 mt-2 mb-4 gap-2">
+                  <CardTitle>게시글 제목</CardTitle>
+                  <p className="text-sm">{DUMMY_TEXT}</p>
+                </section>
+                <p className="text-gray-500">댓글 컴포넌트 들어갈 공간</p>
+              </CardContent>
+            </Card>
           </section>
-          <section className="flex flex-col p-0 mt-2 mb-4 gap-2">
-            <CardTitle>게시글 제목</CardTitle>
-            <p className="text-sm">{DUMMY_TEXT}</p>
-          </section>
-          <p className="text-gray-500">댓글 컴포넌트 들어갈 공간</p>
-        </CardContent>
-      </Card>
+        </TabsContent>
+      </Tabs>
     </main>
   );
 };
