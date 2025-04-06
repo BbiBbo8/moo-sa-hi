e2dca27c3d6e380ce7f61b1cc808e29ada410383
nahyun park <pnh13584@gmail.com>
2025-04-06 21:24:38 +0900

    test: 테스트 코드 작성 #29

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ProfileTabs from "@/components/ProfileTabs";
+import ProfileCard from "@/components/ProfileCard";
 
 const ProfilePage = () => {
   return (
-    <section className="text-center">
-      {/* 아바타 컴포넌트와 카드 컴포넌트 추가후 수정 */}
-      <section>
-        <div>닉네임</div>
-        <div>아바타</div>
-      </section>
+    <section>
+    <ProfileCard/>
       {/* 테스트 코드용. 아래에 탭이 들어갑니다 */}
       <ProfileTabs />
     </section>
