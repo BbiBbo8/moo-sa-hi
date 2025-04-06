b1986088488c747301d2206dbb102bb7d433bc8f
nahyun park <pnh13584@gmail.com>
2025-04-06 10:51:18 +0900

    test: 테스트 코드 작성 #49

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import ProfileTabs from "@/components/ProfileTabs";
+import ProfileEditPop from "@/components/ProfileEditPop";
 
 const ProfilePage = () => {
   return (
     <>
       <div>page</div>
+      <ProfileEditPop/>
       {/* 테스트 코드용. 아래에 탭이 들어갑니다 */}
       <ProfileTabs />
     </>
