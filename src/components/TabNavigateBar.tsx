3b724f35919a2feee2146246eb718f694da34595
nahyun park <pnh13584@gmail.com>
2025-04-06 17:17:30 +0900

    feat: Link 컴포넌트 생성 #9

--- src/components/TabNavigateBar.tsx
+++ src/components/TabNavigateBar.tsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { FileText, Home, Map, MapPin, UserRound } from 'lucide-react';
+import Link from 'next/link';
 
 function TabNavigateBar() {
   return (
     <div className='p-1 inline-flex w-full items-center justify-between text-center relative'>
-        <div>
+        <Link href={"/"}>
             <Home className='w-24 text-center'/>
             <h5>홈</h5>
-        </div>
-        <div>
+        </Link>
+        <Link href={"/map"}>
             <MapPin className='w-24 text-center'/>
             <h5>주변 대피소</h5>
-        </div>
-        <div>
+        </Link>
+        <Link href={"/community-shelter"}>
             <FileText className='w-24 text-center'/>
             <h5>커뮤니티</h5>
-        </div>
-        <div>
+        </Link>
+        <Link href={"/profile"}>
             <UserRound className='w-24 text-center'/>
             <h5>마이</h5>
-        </div>
+        </Link>
         </div>
   )
 }
