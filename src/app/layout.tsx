c8c2042af8ccac7bd72e932a9f25a0ca31e4d49c
nahyun park <pnh13584@gmail.com>
2025-04-06 16:48:55 +0900

    fix: 주석 오류 수정 #9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import TabNavigateBar from "@/components/TabNavigateBar";
 
 export const metadata: Metadata = {
   title: "무사히",
-  description: "대피소 정보 공유 사이트" /* 임의대로 작성한 내용. 추후 수정 */,
+  description: "대피소 정보 공유 사이트" /* 임의대로 작성한 내용. 추후 수정 */
 };
 
 export default function RootLayout({
