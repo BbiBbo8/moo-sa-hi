1b6b04cf31875b4c6a13d0168bd815629f90f0a6
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-04-06 22:44:24 +0900

    comment: 지도페이지 기능 내용 주석 설명

--- src/components/map/ShelterList.tsx
+++ src/components/map/ShelterList.tsx
@@ -19,7 +19,21 @@ const dummyShelters = [
     distance: 2.3,
   },
 ];
-
+/*
+* [패널 전체를 감싸는 컨테이너]
+* - absolute: 부모 기준으로 절대 위치
+* - bottom-0: 아래에 붙이기
+* - w-full: 너비 전체
+* - max-w-[393px]: 최대 너비 제한 (모바일 사이즈)
+* - bg-white: 배경 흰색
+* - p-4: 내부 여백 16px
+* - rounded-t-2xl: 위쪽 모서리 둥글게
+* - shadow-md: 중간 그림자
+* - z-20: 위쪽에 쌓이도록 z-index 설정
+* - transition-transform duration-300: transform 전환 애니메이션
+* - translate-y-0: 기본 위치 (열림)
+* - translate-y-[70%]: 아래로 70% 숨겨짐 (닫힘)
+*/
 const ShelterList = () => {
     // 하단 패널이 열려있는지 여부를 확인하는 상태 값
   const [isOpen, setIsOpen] = useState(false);
