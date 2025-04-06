b2cb4e8632343a11e888b8efc58ab91554cdca62
nahyun park <pnh13584@gmail.com>
2025-04-07 00:25:53 +0900

    style: card 내 태그 수정 #29

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,32 +1,35 @@
 import React from "react";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 function ProfileCard() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>닉네임</CardTitle>
+    <div className="flex flex-col gap-6 rounded-xl border py-6 shadow-sm">
+      <div className="gap-2">
+        <h3 className="text-lg font-bold">닉네임</h3>
         {/* 이외 추가 설명이 있다면 들어갈 곳 */}
-        <CardDescription>추가 설명</CardDescription>
-        {/* 아바타 들어갈 곳 */}
-        <Avatar>
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-      </CardHeader>
-      <CardContent>
-        <p>작성 게시글 수</p>
-        <p>참여 게시글 수</p>
-        <p>저장 게시글 수</p>
-      </CardContent>
-    </Card>
+        <span className="text-xs font-light">추가 설명</span>
+      </div>
+      {/* 아바타 들어갈 곳 */}
+      <Avatar>
+        <AvatarImage src="https://github.com/shadcn.png" />
+        <AvatarFallback>아바타</AvatarFallback>
+      </Avatar>
+      {/* 해당 기능은 도전 기능입니다 뼈대만 존재 */}
+      <div className="flex flex-row justify-evenly">
+        <div>
+          <h5 className="text-xs font-extrabold">작성 게시글 수</h5>
+          <p>22</p>
+        </div>
+        <div>
+          <h5 className="text-xs font-extrabold">참여 게시글 수</h5>
+          <p>11</p>
+        </div>
+        <div>
+          <h5 className="text-xs font-extrabold">저장 게시글 수</h5>
+          <p>1</p>
+        </div>
+      </div>
+    </div>
   );
 }
 
