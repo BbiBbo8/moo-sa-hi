8fa3c8ba44076379c532ade082915f00e327fccc
nahyun park <pnh13584@gmail.com>
2025-04-06 17:06:48 +0900

    style: lucid-icon 추가

--- src/components/TabNavigateBar.tsx
+++ src/components/TabNavigateBar.tsx
@@ -1,12 +1,26 @@
 import React from 'react'
-import { Home } from 'lucide-react';
+import { FileText, Home, Map, MapPin, UserRound } from 'lucide-react';
 
 function TabNavigateBar() {
   return (
-    <div className='w-full relative'>
+    <div className='p-1 inline-flex w-full items-center justify-between text-center relative'>
         <div>
-        <Home/>
-        </div></div>
+            <Home className='w-24 text-center'/>
+            <h5>홈</h5>
+        </div>
+        <div>
+            <MapPin className='w-24 text-center'/>
+            <h5>주변 대피소</h5>
+        </div>
+        <div>
+            <FileText className='w-24 text-center'/>
+            <h5>커뮤니티</h5>
+        </div>
+        <div>
+            <UserRound className='w-24 text-center'/>
+            <h5>마이</h5>
+        </div>
+        </div>
   )
 }
 
