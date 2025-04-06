afa199a143a258c01e9208d73c9c711a1bd04de8
nahyun park <pnh13584@gmail.com>
2025-04-06 19:39:34 +0900

    fix: 시맨틱 태그 오류 수정 #9

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,7 @@ import TabNavigateBar from "@/components/TabNavigateBar";
 
 export const metadata: Metadata = {
   title: "무사히",
-  description: "대피소 정보 공유 사이트" /* 임의대로 작성한 내용. 추후 수정 */
+  description: "대피소 정보 공유 사이트" /* 임의대로 작성한 내용. 추후 수정 */,
 };
 
 export default function RootLayout({
@@ -14,8 +14,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body>{children}</body>
-      <TabNavigateBar/>
+      <body>
+        <main>{children}</main>
+        <TabNavigateBar />
+      </body>
     </html>
   );
 }
