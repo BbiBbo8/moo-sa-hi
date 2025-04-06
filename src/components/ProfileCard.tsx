17dff38c8d0dfcde51a03ad2ea3f79aafd0246eb
nahyun park <pnh13584@gmail.com>
2025-04-06 23:17:40 +0900

    feat: avatar 컴포넌트 추가 #29

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -1,27 +1,33 @@
-import React from 'react'
+import React from "react";
 import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "./ui/avatar";
 
 function ProfileCard() {
   return (
     <Card>
-  <CardHeader>
-    <CardTitle>Card Title</CardTitle>
-    <CardDescription>Card Description</CardDescription>
-  </CardHeader>
-  <CardContent>
-    <p>Card Content</p>
-  </CardContent>
-</Card>
-
-  )
+      <CardHeader>
+        <CardTitle>닉네임</CardTitle>
+        {/* 이외 추가 설명이 있다면 들어갈 곳 */}
+        <CardDescription>추가 설명</CardDescription>
+        {/* 아바타 들어갈 곳 */}
+        <Avatar>
+          <AvatarImage src="https://github.com/shadcn.png" />
+          <AvatarFallback>CN</AvatarFallback>
+        </Avatar>
+      </CardHeader>
+      <CardContent>
+        <p>작성 게시글 수</p>
+        <p>참여 게시글 수</p>
+        <p>저장 게시글 수</p>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default ProfileCard
+export default ProfileCard;
