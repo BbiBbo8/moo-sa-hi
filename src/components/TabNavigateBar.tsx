8f86d46fd33b5336d4a8306e34452964474eefe0
nahyun park <pnh13584@gmail.com>
2025-04-06 16:42:51 +0900

    feat: TabNavigateBar 컴포넌트 생성

---
+++ src/components/TabNavigateBar.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Home } from 'lucide-react';
+
+function TabNavigateBar() {
+  return (
+    <section className='w-full relative'>
+        <div>
+        <Home/>
+        </div></section>
+  )
+}
+
+export default TabNavigateBar
