87e3ff8ebc06e7b5062bed7b440731fb1dd5adfe
nahyun park <pnh13584@gmail.com>
2025-04-07 11:57:01 +0900

    style: 아바타 border 수정 #29

--- src/components/ProfileCard.tsx
+++ src/components/ProfileCard.tsx
@@ -6,13 +6,14 @@ function ProfileCard() {
     <div className="flex flex-col gap-3 rounded-xl border py-4 shadow-sm">
       <div className="gap-2">
         <h3 className="text-lg font-bold">닉네임</h3>
+        <h3 className="text-md">@email.com</h3>
         {/* 이외 추가 설명이 있다면 들어갈 곳 */}
         <span className="text-xs font-light">추가 설명</span>
       </div>
       {/* 아바타 들어갈 곳 */}
-      <figure>
-    <div>아바타</div>
-    <ProfileEditPop/>
+      <figure className="h-24 w-24 rounded-full border bg-gray-200">
+        <div>아바타</div>
+        <ProfileEditPop />
       </figure>
       {/* 해당 기능은 도전 기능입니다 뼈대만 존재 */}
       <div className="flex flex-row justify-evenly">
