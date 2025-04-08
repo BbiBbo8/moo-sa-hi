ba151246c278704b96444dff9125c6fe38968670
nahyun park <pnh13584@gmail.com>
2025-04-08 11:02:31 +0900

    feat: user 정보 불러오기 #19

--- src/supabase/getUser.ts
+++ src/supabase/getUser.ts
@@ -2,8 +2,16 @@ import React from "react";
 import { supabase } from "./client";
 
 const getUser = async () => {
-  const { data: users, error } = await supabase.from("users").select("*");
+  const { data: users, error } = await supabase
+    .from("users")
+    .select("*")
+    .eq("nickname", "닉네임뭐하지") /* 테스트용 코드 */
+    .single();
 
+  if (error) {
+    console.error("사용자 데이터 가져오기 실패:", error);
+    return null;
+  }
   return users;
 };
 
