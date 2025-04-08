689614e2c1d57d577643e6e37d6b2640b3bb79d6
33hyun <lggx12345@gmail.com>
2025-04-08 16:40:29 +0900

    feat: 랜딩 페이지에 배너 섹션 추가 및 상단 배너와 지도 미리보기 설명 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import AlertBanner from "@/components/landing/AlertBanner";
 import MapPreview from "@/components/landing/MapPreview";
-
+import LandingBanner from "@/components/landing/BannerSection";
 
 export default function HomePage() {
   return (
     <main className="px-4 py-6 space-y-8">
+      <>상단 배너</>
       <AlertBanner />
+      <>지도 미리보기 클릭시 = map페이지로 이동</>
       <MapPreview />
-      
+      <p>배너섹션 = 재난안전포털 외부 url 연결</p>
+      <LandingBanner />
     </main>
   );
 }
