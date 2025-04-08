e1ff6828e5482073c926ecc3106e3d73978ca49c
nahyun park <pnh13584@gmail.com>
2025-04-08 17:23:40 +0900

    rename: provider 폴더로 이동 #55

--- src/providers/providers.tsx
+++ src/providers/providers.tsx
@@ -3,12 +3,12 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode, useState } from "react";
 
-export default function Providers({ children }: { children: ReactNode }) {
+const ReactQueryProvider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      {children}
-    </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
   );
-}
+};
+
+export default ReactQueryProvider;
