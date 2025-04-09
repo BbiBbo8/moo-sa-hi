6973d86231384b77e01bf7b9e91c5add8cfc3941
nahyun park <pnh13584@gmail.com>
2025-04-09 20:42:18 +0900

    fix: 변수를 직관적으로 명명 #28

--- src/supabase/getUserData.ts
+++ src/supabase/getUserData.ts
@@ -7,21 +7,21 @@ const getUserData = () => {
   const supabase = createClient();
 
   const fetchUser = async () => {
+    // login한 유저의 정보를 쿠키를 참조해 가져오기
     const {
       data: { user },
     } = await supabase.auth.getUser();
-    // 이후 로그인된 user id와 맞는 id 불러오는 로직으로 변경 예정
-    const { data: users, error } = await supabase
+    const { data: userMetaData, error } = await supabase
       .from("users")
       .select("*")
-      .eq("id", `${user?.id}`) /* 테스트용 코드 */
+      .eq("id", `${user?.id}`) /* 가져온 유저를 참조해 table 정보를 가져오기 */
       .single();
 
     if (error) {
       console.error("사용자 데이터 가져오기 실패:", error);
       return null;
     }
-    return { user, users };
+    return { user, userMetaData };
   };
   return fetchUser();
 };
