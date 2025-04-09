17a01789c3d9346afcf75c641b6cbbac8765f9d6
nahyun park <pnh13584@gmail.com>
2025-04-09 16:44:26 +0900

    feat: suapase auth를 이용해 로그인 유저 정보 접근 #28

--- src/supabase/getUserData.ts
+++ src/supabase/getUserData.ts
@@ -1,20 +1,34 @@
-import React from "react";
+"use clinet";
+
+import React, { useEffect } from "react";
 import createClient from "./client";
 
-const getUserData = async () => {
-  const supabase = createClient();
-  // 이후 로그인된 user id와 맞는 id 불러오는 로직으로 변경 예정
-  const { data: users, error } = await supabase
-    .from("users")
-    .select("*")
-    .eq("nickname", "닉네임뭐하지") /* 테스트용 코드 */
-    .single();
+const getUserData = () => {
+  useEffect(() => {
+    const supabase = createClient();
 
-  if (error) {
-    console.error("사용자 데이터 가져오기 실패:", error);
-    return null;
-  }
-  return users;
+    const getUser = async () => {
+      const {
+        data: { user },
+      } = await supabase.auth.getUser();
+      // 이후 로그인된 user id와 맞는 id 불러오는 로직으로 변경 예정
+      const { data: users, error } = await supabase
+        .from("users")
+        .select("*")
+        .eq("id", `${user?.id}`) /* 테스트용 코드 */
+        .single();
+      console.log(user);
+      console.log(users);
+      return { user, users };
+    };
+
+    // if (error) {
+    //   console.error("사용자 데이터 가져오기 실패:", error);
+    //   return null;
+    // }
+    getUser();
+  }, []);
+  return;
 };
 
 export default getUserData;
