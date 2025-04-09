0ebf34b5053a8066bfe463aaeeb871bd6eaa35fc
nahyun park <pnh13584@gmail.com>
2025-04-09 19:57:54 +0900

    fix: 함수 오류 수정 #28

--- src/supabase/getUserData.ts
+++ src/supabase/getUserData.ts
@@ -4,32 +4,29 @@ import React, { useEffect } from "react";
 import createClient from "./client";
 
 const getUserData = () => {
-  useEffect(() => {
-    const supabase = createClient();
+  const supabase = createClient();
 
-    const getUser = async () => {
-      const {
-        data: { user },
-      } = await supabase.auth.getUser();
-      // 이후 로그인된 user id와 맞는 id 불러오는 로직으로 변경 예정
-      const { data: users, error } = await supabase
-        .from("users")
-        .select("*")
-        .eq("id", `${user?.id}`) /* 테스트용 코드 */
-        .single();
+  const getUser = async () => {
+    const {
+      data: { user },
+    } = await supabase.auth.getUser();
+    // 이후 로그인된 user id와 맞는 id 불러오는 로직으로 변경 예정
+    const { data: users, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("id", `${user?.id}`) /* 테스트용 코드 */
+      .single();
 
-      if (error) {
-        console.error("사용자 데이터 가져오기 실패:", error);
-        return null;
-      }
+    if (error) {
+      console.error("사용자 데이터 가져오기 실패:", error);
+      return null;
+    }
 
-      console.log(user);
-      console.log(users);
-      return { user, users };
-    };
-    getUser();
-  }, []);
-  return;
+    console.log("auth 정보=", user);
+    console.log("auth의 metadata=", users);
+    return { user, users };
+  };
+  return getUser();
 };
 
 export default getUserData;
