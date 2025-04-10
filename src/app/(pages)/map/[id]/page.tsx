9ad022f83410961c605c3da879797486b991edcc
nahyun park <pnh13584@gmail.com>
2025-04-10 16:30:54 +0900

    feat: 디테일 내용 추가 #95

--- src/app/(pages)/map/[id]/page.tsx
+++ src/app/(pages)/map/[id]/page.tsx
@@ -3,15 +3,17 @@ import React from "react";
 
 const ShelterDetailPage = () => {
   return (
-    <>
-      <div>지도</div>
-      <h3 className="text-4xl">대피소 명</h3>
+    <section className="flex flex-col gap-2">
+      <div className="bg-accent h-40 w-full">지도</div>
+      <h3 className="text-4xl font-semibold">대피소 명</h3>
       <ShelterBanner />
-      <div>장소 공유</div>
-      <div>길 안내</div>
+      <div className="flex-row">
+        <div>장소 공유</div>
+        <div>길 안내</div>
+      </div>
       <p>대피소 정보 오류는 행정안전부 콜센터로 제보 바랍니다.</p>
       <p>02-2100-3399</p>
-    </>
+    </section>
   );
 };
 
