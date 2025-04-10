972f320ef80c5857089dafcde190d7f1a944f7bb
nahyun park <pnh13584@gmail.com>
2025-04-10 14:43:44 +0900

    feat: shelterBanner 보이기 #95

--- src/app/(pages)/map/[id]/page.tsx
+++ src/app/(pages)/map/[id]/page.tsx
@@ -1,7 +1,18 @@
+import ShelterBanner from "@/components/map/ShelterBanner";
 import React from "react";
 
 const ShelterDetailPage = () => {
-  return <div>page</div>;
+  return (
+    <>
+      <div>지도</div>
+      <h3 className="text-4xl">대피소 명</h3>
+      <ShelterBanner />
+      <div>장소 공유</div>
+      <div>길 안내</div>
+      <p>대피소 정보 오류는 행정안전부 콜센터로 제보 바랍니다.</p>
+      <p>02-2100-3399</p>
+    </>
+  );
 };
 
 export default ShelterDetailPage;
