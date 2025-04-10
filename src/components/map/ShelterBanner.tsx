a7b7eba1bc9a82706877286497b1afeeb7e1b8eb
nahyun park <pnh13584@gmail.com>
2025-04-10 19:56:37 +0900

    feat: 클릭 시 복사 이벤트 연결 #95

--- src/components/map/ShelterBanner.tsx
+++ src/components/map/ShelterBanner.tsx
@@ -1,13 +1,25 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+"use client";
+
+import React from "react";
+import { Copy } from "lucide-react";
 
 const ShelterBanner = () => {
+  const handleCopyText = async (text: string) => {
+    try {
+      await navigator.clipboard.writeText(text);
+      console.log("클릭 이벤트");
+      alert("복사 완료!");
+    } catch (e) {
+      alert("복사 실패!");
+    }
+  };
   return (
-    <Alert>
-      <AlertTitle>Heads up!</AlertTitle>
-      <AlertDescription>
-        You can add components and dependencies to your app using the cli.
-      </AlertDescription>
-    </Alert>
+    <div className="text-md flex w-full justify-evenly rounded-lg border px-4 py-3 text-center">
+      <button onClick={() => handleCopyText("서울특별시 어쩌고")}>
+        <Copy />
+      </button>
+      <h5>서울특별시 지역동 어느길 0000-0000</h5>
+    </div>
   );
 };
 
