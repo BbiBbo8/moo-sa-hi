7e616827199d857f01eadfce19e25e93c0732a30
nahyun park <pnh13584@gmail.com>
2025-04-11 12:12:44 +0900

    feat: input 태그 생성 #23

--- src/components/community/CommunityComments.tsx
+++ src/components/community/CommunityComments.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const CommunityComments = () => {
-  return <div>CommunityComments</div>;
+  return <input placeholder="댓글을 입력해주세요." />;
 };
 
 export default CommunityComments;
