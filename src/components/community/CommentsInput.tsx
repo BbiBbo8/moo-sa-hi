e4fcaf3f064d1049b04975b482042bb4d6d40d4b
nahyun park <pnh13584@gmail.com>
2025-04-11 12:24:08 +0900

    rename: CommentsInput으로 변경

--- src/components/community/CommentsInput.tsx
+++ src/components/community/CommentsInput.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-const CommunityComments = () => {
+const CommentsInput = () => {
   return <input placeholder="댓글을 입력해주세요." />;
 };
 
-export default CommunityComments;
+export default CommentsInput;
