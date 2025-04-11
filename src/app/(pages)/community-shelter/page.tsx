d850b2f23650d60d660f7e478712b06f77e93fb4
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-11 14:03:30 +0900

    refactor: pr 피드백 반영 리팩토링

--- src/app/(pages)/community-shelter/page.tsx
+++ src/app/(pages)/community-shelter/page.tsx
@@ -26,31 +26,36 @@ const CommunityShelterPage = () => {
     return <p>에러 발생: {error.message}</p>;
   }
 
+  const dailyPageSwitch = "일상";
+  const shelterPageSwitch = "대피소";
+  const searchPlaceholder = "키워드 검색";
+  const instruction = "대피소 관련 경험과 정보를 솔직하게 공유해주세요.";
+
   return (
-    <main className="flex h-screen min-w-screen flex-col items-center p-4">
-      {/* 커뮤니티 이동 탭 */}
-      <section className="top-3 left-5 mb-10 flex flex-row items-center gap-4">
-        <Link href="/community-shelter">대피소</Link>
-        <Link href="/community-daily">일상</Link>
+    <main className="relative flex h-screen min-w-screen flex-col items-center justify-center p-4">
+      {/* 고민 사항 : 윗부분 (로고, 검색창, 안내문)은 화면에 fixed로 고정하는 게 좋을까요? 불필요한 스크롤를 줄일 수 있는 점이 UX 좋을 것 같긴 합니다 */}
+      <section className="absolute top-3 left-5 flex flex-row items-center gap-4">
+        <Link href="/community-shelter">{shelterPageSwitch}</Link>
+        <Link href="/community-daily">{dailyPageSwitch}</Link>
         {/* 고민 사항 : 이미 만들어진 검색창 컴포넌트 사용할까 고민 중. 그런데 디테일이나 모달창이 나오는 부분등에서 차이가 꽤 많아서 따로 컴포넌트를 만들어야하나 고민됨.*/}
         {/* <div className="[&>*]:w-60">
           <InputSearch />
         </div> */}
-        <div className="relative h-10 w-60 rounded-md bg-gray-100">
+        <div className="relative h-10 w-64 rounded-md bg-gray-100">
           <Search className="absolute top-1/2 right-3 -translate-y-1/2 text-gray-500" />
           <span className="absolute top-1/2 left-3 -translate-y-1/2 text-gray-500">
-            키워드 검색
+            {searchPlaceholder}
           </span>
         </div>
       </section>
 
-      <section>
-        <span className="mb-20 flex h-20 w-80 items-center justify-center bg-gray-200">
-          배너 등이 들어갈 공간
-        </span>
-      </section>
+      <p className="absolute top-16 flex h-12 w-full items-center justify-center gap-2 bg-white">
+        {/* 아이콘 대용 네모 */}
+        <div className="h-5 w-5 bg-gray-200"></div>
+        {instruction}
+      </p>
 
-      <section className="grid w-full grid-cols-2 justify-center gap-3">
+      <section className="absolute top-30 grid w-full grid-cols-2 justify-center gap-3 p-4">
         {shelterPosts?.map(post => {
           return <ShelterPost key={post.id} post={post} />;
         })}
