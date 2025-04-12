c6d9899cb8cb36a76dad200d65682f54765d29a2
33hyun <lggx12345@gmail.com>
2025-04-12 18:35:41 +0900

    refactor: 버튼 컴포넌트로 링크 래핑하여 코드 구조 개선

--- src/components/landing/BannerSection.tsx
+++ src/components/landing/BannerSection.tsx
@@ -5,15 +5,16 @@ export default function LandingBanner() {
     <section className="relative w-full h-[300px] bg-blue-300 flex flex-col justify-center items-center text-center mx-20px">
       <h1 className="text-3xl font-bold mb-2">빠르고 안전한 대피소 찾기</h1>
       <p>근처의 재난 대피소 정보를 한눈에 확인하세요</p>
-      <a
-        href="https://www.safekorea.go.kr"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <Button className="text-white bg-blue-600">
+
+      <Button asChild className="text-white bg-blue-600 mt-4">
+        <a
+          href="https://www.safekorea.go.kr"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           재난안전포털 바로가기
-        </Button>
-      </a>
+        </a>
+      </Button>
     </section>
   );
 }
