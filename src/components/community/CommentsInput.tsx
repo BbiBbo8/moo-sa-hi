b27d33261e017f34958032e6a29ae90e40ce9e8c
nahyun park <pnh13584@gmail.com>
2025-04-12 11:41:04 +0900

    test: 라이브러리 테스트 코드

--- src/components/community/CommentsInput.tsx
+++ src/components/community/CommentsInput.tsx
@@ -1,7 +1,65 @@
+"use client";
+
 import React from "react";
+import { Form, FormControl, FormField } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Input } from "@/components/ui/input";
+import createClient from "@/supabase/client";
+
+const supabase = createClient();
 
 const CommentsInput = () => {
-  return <input placeholder="댓글을 입력해주세요." />;
+  const FormSchema = z.object({
+    contents: z.string().max(30, {
+      message: "댓글의 글자수는 30자를 초과할 수 없습니다.",
+    }),
+  });
+
+  const InputForm = () => {
+    const form = useForm<z.infer<typeof FormSchema>>({
+      resolver: zodResolver(FormSchema),
+      defaultValues: {
+        contents: "",
+      },
+    });
+
+    const onSubmit = async (data: z.infer<typeof FormSchema>) => {
+      try {
+        await supabase
+          .from("comments")
+          .insert({
+            user_id: "someValue",
+            shelter_post_id: "null",
+            daily_post_id: "null",
+            comments: data,
+          })
+          .select();
+
+        alert("저장 완료");
+      } catch (error) {
+        alert("오류 발생");
+      }
+    };
+
+    return (
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FormField
+            control={form.control}
+            name="contents"
+            render={({ field }) => (
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+            )}
+          />
+          <button type="submit">등록</button>
+        </form>
+      </Form>
+    );
+  };
 };
 
 export default CommentsInput;
