17b3c5cfbebd1d4947d0c09478c22a73981ea106
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-04-13 21:50:37 +0900

    fix: 확인하는 useEffect 제거거

--- src/hooks/useShelterMapEffect.ts
+++ src/hooks/useShelterMapEffect.ts
@@ -5,7 +5,7 @@ import { useShelters } from "./shelter/useShelters";
 
 // zustand의 Map과 shelter 데이터 동기화 하는 커스텀 훅
 export const useShelterMapEffect = () => {
-  const { data: shelters = [], isLoading, isError } = useShelters();
+  const { data: shelters = [] } = useShelters();
   const setCenter = useMapStore(state => state.setCenter);
 
   // shelter 데이터가 로드된 후 shelter를 지도의 중심(center)으로 설정
@@ -16,15 +16,4 @@ export const useShelterMapEffect = () => {
       setCenter({ lat: shelter.lat, lng: shelter.lng });
     }
   }, [shelters, setCenter]);
-
-  // 로딩 상태나 에러 상태 처리
-  useEffect(() => {
-    if (isLoading) {
-      console.log("데이터 로딩 중...");
-    }
-
-    if (isError) {
-      throw new Error("shelter 데이터를 불러오지 못했습니다.");
-    }
-  }, [isLoading, isError]);
 };
