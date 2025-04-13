13a4cad96f0a9ed2e6cb550ec860689bdcbd0d20
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-14 00:54:52 +0900

    style: 상세페이지 내용 렌더링과 기본 레이아웃

--- src/components/community/DailyDetailPost.tsx
+++ src/components/community/DailyDetailPost.tsx
@@ -11,19 +11,36 @@ const DailyDetailPost = ({ id }: { id: number }) => {
   if (error) return <p>에러 발생: {error.message}</p>;
   if (!data) return null; // <= 이거 추가
 
-  const formatted = format(new Date(data.created_at), "yyyy.MM.dd");
+  const timeCreated = format(new Date(data.created_at), "yyyy.MM.dd");
 
   return (
-    <main>
-      <h1 className="text-[20px]">{data.title}</h1>
-      <span>{formatted}</span>
-      {/* <Image
+    <main className="m-4 flex flex-col items-center gap-5">
+      <section className="mb-5 w-full flex-col gap-5 not-visited:flex">
+        <section className="flex flex-row items-baseline justify-between">
+          <h1 className="text-[20px]">{data.title}</h1>
+          <span className="text-[14px] text-gray-500">{timeCreated}</span>
+        </section>
+        {/* NOTE: 이미지가 여러 개일 때 부모요소 역할을 할 section태그입니다. */}
+        <section className="flex items-center justify-center border-2 border-gray-200">
+          <figure className="flex h-[350px] w-[350px] items-center justify-center rounded-3xl border-2 border-gray-400">
+            이미지
+            {/* <Image
         src={data.img_url || ""}
         alt="이미지가 없습니다."
         width={300}
         height={200}
       /> */}
-      <p>{data.contents}</p>
+          </figure>
+        </section>
+        <p className="min-h-20 w-full border-2 border-gray-400">
+          {data.contents}
+        </p>
+      </section>
+
+      {/* TEST: 화면에 보이는 회색 줄 부분입니다. 임시로 div태그를 사용했지만 이후 바뀔 예정입니다. 무슨 태그가 좋을까요? */}
+      <div className="h-4 min-w-screen bg-gray-200"></div>
+
+      <section>댓글이 들어갈 공간</section>
     </main>
   );
 };
