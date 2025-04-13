03ffb9be655c654cf2bcc81332c4900f9837d65f
nahyun park <pnh13584@gmail.com>
2025-04-13 17:40:43 +0900

    fix: 사용하지 않는 아바타 삭제

--- src/components/profile/ProfileCard.tsx
+++ src/components/profile/ProfileCard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import ProfileEditPop from "./ProfileEditPop";
-import { Avatar, AvatarImage } from "../ui/avatar";
 import Loading from "@/app/(pages)/Loading";
 import { useUserData } from "@/hooks/useUserData";
 import Error from "@/app/(pages)/Error";
@@ -30,31 +29,9 @@ const ProfileCard = () => {
         </div>
         {/* TEST: 임시 로그아웃 버튼 추가 */}
         <LogoutButton />
-        {/* 아바타 들어갈 곳 */}
-        <Avatar className="size-16">
-          <AvatarImage src={userAuth.user_metadata.avatar_url} />
-        </Avatar>
-        <ProfileEditPop
-          userId={userAuth.id}
-          nickname={user.nickname}
-          avatarUrl={userAuth.user_metadata.avatar_url}
-        />
+        <ProfileEditPop userId={userAuth.id} nickname={user.nickname} />
       </div>
-      {/* 해당 기능은 도전 기능입니다 뼈대만 존재 */}
-      <div className="flex flex-row justify-evenly">
-        <div>
-          <h5 className="text-xs font-extrabold">작성 게시글 수</h5>
-          <p>22</p>
-        </div>
-        <div>
-          <h5 className="text-xs font-extrabold">참여 게시글 수</h5>
-          <p>11</p>
-        </div>
-        <div>
-          <h5 className="text-xs font-extrabold">저장 게시글 수</h5>
-          <p>1</p>
-        </div>
-      </div>
+      {/* 아래의 게시글 수 도전 기능은 일단 구현하지 않음 */}
     </div>
   );
 };
