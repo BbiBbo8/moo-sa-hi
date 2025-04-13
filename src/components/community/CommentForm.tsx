2594a2bf3e4e459ac5cb81892b61ded52a6bc8e9
nahyun park <pnh13584@gmail.com>
2025-04-14 00:03:41 +0900

    fix: 컴포넌트 안에 insert 로직 작성

--- src/components/community/CommentForm.tsx
+++ src/components/community/CommentForm.tsx
@@ -13,7 +13,10 @@ import {
 } from "../ui/form";
 import { Button } from "../ui/button";
 import { Input } from "../ui/input";
-import useInsertComments from "../../hooks/useInsertComment";
+import { usePathname } from "next/navigation";
+import createClient from "@/supabase/client";
+import PATH from "@/constants/PATH";
+import { toast } from "sonner";
 
 const commentSchema = z.object({
   content: z
@@ -31,9 +34,45 @@ const CommentForm = () => {
   } = useForm<CommentFormData>({
     resolver: zodResolver(commentSchema),
   });
+  const pathname = usePathname();
+  const supabase = createClient();
+
+  const handleInsertComments = async ({ content }: { content: string }) => {
+    try {
+      // 현재 pathname을 참조하여 대피소 커뮤니티일 때 대피소 댓글 insert
+      if (pathname.includes(PATH.COMMUNITYSHELTER) === true) {
+        await supabase
+          .from("comments")
+          .insert([
+            {
+              user_id: "11f37be0-4036-467b-b963-11b744903d1c" /* 확인용 임시 */,
+              shelter_post_id: 4 /* 확인용 임시 */,
+              daily_post_id: null,
+              comments: content,
+            },
+          ])
+          .select();
+        toast.info("댓글 작성 완료!");
+      } else {
+        // 이외일 때 일상 댓글 insert
+        await supabase
+          .from("comments")
+          .insert({
+            user_id: "11f37be0-4036-467b-b963-11b744903d1c" /* 확인용 임시 */,
+            shelter_post_id: null,
+            daily_post_id: 1 /* 확인용 임시 */,
+            comments: content,
+          })
+          .select();
+        toast.info("댓글 작성 완료!");
+      }
+    } catch (error) {
+      toast.error("댓글 작성 오류 발생");
+    }
+  };
 
   const onSubmit = (content: CommentFormData) => {
-    useInsertComments(content);
+    handleInsertComments(content);
     reset();
   };
 
