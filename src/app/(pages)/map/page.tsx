9797ae3c6805516799742ecb75a381d1aeefe926
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-04-13 22:15:35 +0900

    rename: page 서버 컴포넌트 수정정

--- src/app/(pages)/map/page.tsx
+++ src/app/(pages)/map/page.tsx
@@ -1,23 +1,7 @@
-"use client";
-
-import MainMap from "@/components/map/MainMap";
-import ShelterDrawer from "@/components/map/ShelterDrawer";
-import InputSearch from "@/components/map/InputSearch";
+import MapClient from "@/components/map/MapClient";
 
 const MapPage = () => {
-  return (
-    <div className="relative mx-auto flex h-[852px] max-w-[393px] flex-col items-center justify-center overflow-hidden">
-      <div className="relative flex flex-row justify-center">
-        <div className="flex justify-center">
-          <InputSearch />
-        </div>
-      </div>
-      <MainMap />
-      <div className="flex justify-center">
-        <ShelterDrawer />
-      </div>
-    </div>
-  );
+  return <MapClient />;
 };
 
 export default MapPage;
