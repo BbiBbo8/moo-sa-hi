49714b0cf3c2f54060678f8ae85d944c1f7ba71e
33hyun <lggx12345@gmail.com>
2025-04-14 20:29:54 +0900

    feat: <a>태그로 외부 카카오맵 연결 기능 추가

--- src/components/map/ShelterExtraFeature.tsx
+++ src/components/map/ShelterExtraFeature.tsx
@@ -8,9 +8,7 @@ interface ShelterExtraFeatureProps {
   address?: string;
 }
 
-const ShelterExtraFeature = ({
-  address
-}: ShelterExtraFeatureProps) => {
+const ShelterExtraFeature = ({ address }: ShelterExtraFeatureProps) => {
   // 복사 이벤트 발생 함수
   const handleCopyText = async (text: string) => {
     try {
@@ -24,26 +22,29 @@ const ShelterExtraFeature = ({
     <>
       <div className="text-md flex w-full justify-evenly rounded-lg border px-4 py-3 text-center">
         {/* 클릭 시 텍스트를 복사 */}
-        <button
-          onClick={() =>
-            handleCopyText(address ?? "주소 정보 없음")
-          }
-        >
+        <button onClick={() => handleCopyText(address ?? "주소 정보 없음")}>
           <Copy />
         </button>
         {/* 트렁케이트: 주소가 길 경우 생략 줄 넘침 방지 ex)00동 ... */}
         <h5 className="truncate">{address ?? "정보 없음"}</h5>
       </div>
-      {/* 장소 공유와 길 안내는 추후 도전 기능으로 추가 */}
+      
       <div className="flex justify-evenly gap-2 text-center">
+        {/* 장소공유 */}
         <button className="text-md flex w-1/2 rounded-lg border px-4 py-3">
           <Share2 className="w-12" />
           <p>장소 공유</p>
         </button>
-        <button className="text-md flex w-1/2 rounded-lg border px-4 py-3">
-          <Navigation className="w-12" />
+        {/* 카카오맵 길 안내 */}
+        <a
+          href={`https://map.kakao.com/?q=${encodeURIComponent(address ?? "")}`}
+          target="_blank"
+          rel="noopener noreferrer"
+          className="text-md flex w-1/2 items-center justify-center gap-2 rounded-lg border px-4 py-3"
+        >
+          <Navigation className="w-6" />
           <p>길 안내</p>
-        </button>
+        </a>
       </div>
     </>
   );
