defcc1af9c7e1c3072dbe59f148ce7c811cb22e4
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-14 17:50:14 +0900

    style: 유용해요와 신고하기 버튼 레이아웃

--- src/components/community/DailyDetailPost.tsx
+++ src/components/community/DailyDetailPost.tsx
@@ -8,7 +8,7 @@ import Error from "@/app/(pages)/Error";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
 import PATH from "@/constants/PATH";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import CommentForm from "./form/CommentForm";
 
 const DailyDetailPost = ({ id }: { id: number }) => {
@@ -38,8 +38,8 @@ const DailyDetailPost = ({ id }: { id: number }) => {
 
   return (
     <section className="m-4 flex flex-col items-center gap-5">
-      <article className="mb-5 w-full flex-col gap-5 not-visited:flex">
-        <header className="flex flex-row items-baseline justify-between">
+      <article className="mb-5 w-full flex-col items-center gap-5 not-visited:flex">
+        <header className="flex w-full flex-row items-baseline justify-between">
           <h1 className="text-[20px]">{data.title}</h1>
           <span className="text-[14px] text-gray-500">{timeCreated}</span>
         </header>
@@ -59,6 +59,16 @@ const DailyDetailPost = ({ id }: { id: number }) => {
         <p className="min-h-20 w-full border-2 border-gray-400">
           {data.contents}
         </p>
+
+        <button className="flex h-[40px] w-[120px] items-center justify-center rounded-sm bg-gray-200 text-sm">
+          유용해요
+        </button>
+        <button
+          type="button"
+          className="justify-en items-end bg-blue-200 px-2 py-1 text-[12px]"
+        >
+          신고하기
+        </button>
       </article>
 
       {/* TEST: 화면에 보이는 회색 줄 부분입니다. 임시로 div태그를 사용했지만 이후 바뀔 예정입니다. 무슨 태그가 좋을까요? */}
