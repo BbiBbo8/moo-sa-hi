adfa4f9d701aad60d2cce1536652d359ae4372e2
nahyun park <pnh13584@gmail.com>
2025-04-14 23:23:54 +0900

    fix: params 오류 수정

--- src/app/(pages)/community-daily/[id]/page.tsx
+++ src/app/(pages)/community-daily/[id]/page.tsx
@@ -1,10 +1,20 @@
+import CommentList from "@/components/community/CommentList";
 import DailyDetailPost from "@/components/community/DailyDetailPost";
+import CommentForm from "@/components/community/form/CommentForm";
 
-const DailyDetailPage = async ({ params }: { params: { id: string } }) => {
-  const id = Number(params.id);
+const DailyDetailPage = async ({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) => {
+  const { id } = await params; // NOTE : Promise로 받기 때문에 async/await 필요
+  const numericId = Number(id);
+
   return (
     <main>
       <DailyDetailPost id={2} />
+      <CommentForm postId={numericId} />
+      <CommentList postId={numericId} />
     </main>
   );
 };
