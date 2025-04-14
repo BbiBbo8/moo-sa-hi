c28d955bc5d62d324027b9acc2742586e2221fc7
nahyun park <pnh13584@gmail.com>
2025-04-15 00:08:18 +0900

    feat: 작성 게시글 불러오기

--- src/components/profile/ProfileTabs.tsx
+++ src/components/profile/ProfileTabs.tsx
@@ -1,4 +1,5 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import WrittenPost from "./WrittenPost";
 
 const ProfileTabs = () => {
   return (
@@ -10,7 +11,9 @@ const ProfileTabs = () => {
         <TabsTrigger value="useful">저장된 게시글</TabsTrigger>
       </TabsList>
       {/* 트리거에 맞는 내용 */}
-      <TabsContent value="post">여기에 작성한 게시글이 표시됩니다.</TabsContent>
+      <TabsContent value="post">
+        <WrittenPost />
+      </TabsContent>
       <TabsContent value="comment">
         여기에 댓글을 단 게시글이 표시됩니다.
       </TabsContent>
