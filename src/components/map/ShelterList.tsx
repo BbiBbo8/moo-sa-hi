51b33b691aadbd3c4ec6858ccbad381cc30e3390
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-04-15 00:13:09 +0900

    feat: 대피소 디테일 페이지 이동 링크 대피소 API name으로로

--- src/components/map/ShelterList.tsx
+++ src/components/map/ShelterList.tsx
@@ -1,4 +1,5 @@
 import { useMarkerStore } from "@/store/useMarkerStore";
+import Link from "next/link";
 
 const ShelterList = () => {
   const markedShelter = useMarkerStore(state => state.markedShelter); // MarkerStore의 저장된 데이터 불러오기
@@ -12,10 +13,14 @@ const ShelterList = () => {
           className="mb-2 flex items-center justify-between p-3"
         >
           {/* 대피소 이름 및 주소 정보 */}
-          <div className="flex flex-col gap-1">
+          {/* encodeURIComponent는 URL에 사용할 수 없는 특수 문자나 공백을 안전한 형태로 변환해주는 함수입니다. */}
+          <Link
+            className="flex flex-col gap-1"
+            href={`/map/${encodeURIComponent(shelter.name)}`}
+          >
             <h5 className="text-md font-semibold">{shelter.name}</h5>
             <span className="text-xs text-gray-500">{shelter.address}</span>
-          </div>
+          </Link>
         </div>
       ))}
     </div>
