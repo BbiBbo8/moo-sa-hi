b2ab90cece6a59ed6dcf40c8160e5ec258c2de18
nahyun park <pnh13584@gmail.com>
2025-04-14 17:49:15 +0900

    feat: 넘긴 id 받기

--- src/components/community/CommentList.tsx
+++ src/components/community/CommentList.tsx
@@ -7,23 +7,31 @@ import { useUserData } from "@/hooks/useUserData";
 import Loading from "@/app/(pages)/Loading";
 import Error from "@/app/(pages)/Error";
 import createClient from "@/supabase/client";
+import { useComments } from "@/hooks/useComments";
 
-const CommentList = () => {
+const CommentList = ({ postId }: { postId: number }) => {
   const { data, error, isLoading } = useUserData();
   const user = data?.user;
 
+  const {
+    data: CommentData,
+    error: commnetError,
+    isLoading: isCommentLoading,
+  } = useComments(postId);
+  const comments = CommentData;
+
   // 로딩 중일 때 로딩중 컴포넌트 표시
-  if (isLoading) {
+  if (isLoading || isCommentLoading) {
     return <Loading />;
   }
   // 불러오기 오류일 때 오류 컴포넌트 표시
-  if (error) {
+  if (error || commnetError) {
     return <Error />;
   }
 
   //   댓글 작성자 여부 확인
   const isOwned = () => {
-    if (user?.id === comments.user_id) {
+    if (user?.id === comments?.user_id) {
       return true;
     } else {
       return false;
