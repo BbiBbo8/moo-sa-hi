e2318f670751a776a87f0948323e34febd985d31
33hyun <lggx12345@gmail.com>
2025-04-15 01:11:24 +0900

    feat: 재난 알림 API에서 반환되는 데이터 수를 10에서 1000으로 증가시키고 정렬 기능 추가(최신순)

--- src/app/api/fetchDisasterAlert.ts
+++ src/app/api/fetchDisasterAlert.ts
@@ -10,15 +10,16 @@ const fetchDisasterAlert = async (): Promise<DisasterAlert[]> => {
   const res = await axios.get(apiBase, {
     params: {
       serviceKey: apiKey,
+      pageNo: 30, //30페이지부터 최신 데이터 (추후에 자동으로 최신화 방법 찾기)
       returnType: "json",
-      numOfRows: 10,
+      numOfRows: 1000,
     },
   });
 
   const items = res.data.body;
   if (!Array.isArray(items)) throw new Error("데이터 형식 오류");
 
-  return items
+  const filtered = items
     .filter(item => item.DST_SE_NM !== "기타")
     .map(item => ({
       id: item.SN,
@@ -26,7 +27,10 @@ const fetchDisasterAlert = async (): Promise<DisasterAlert[]> => {
       createdAt: item.CRT_DT,
       region: item.RCPTN_RGN_NM,
       disasterType: item.DST_SE_NM,
-    }));
+    }))
+    .sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
+
+  return filtered;
 };
 
 export default fetchDisasterAlert;
