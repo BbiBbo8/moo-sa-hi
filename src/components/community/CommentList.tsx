458002fc32a91935ff84654fac776c8a8aa68a2c
nahyun park <pnh13584@gmail.com>
2025-04-14 11:49:37 +0900

    feat: CommentList 파일 생성

---
+++ src/components/community/CommentList.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import React from "react";
+import { Card, CardContent, CardDescription } from "../ui/card";
+import { Button } from "../ui/button";
+import { useUserData } from "@/hooks/useUserData";
+import Loading from "@/app/(pages)/Loading";
+import Error from "@/app/(pages)/Error";
+
+const CommentList = () => {
+  const { data, error, isLoading } = useUserData();
+  const user = data?.user;
+
+  // 로딩 중일 때 로딩중 컴포넌트 표시
+  if (isLoading) {
+    return <Loading />;
+  }
+  // 불러오기 오류일 때 오류 컴포넌트 표시
+  if (error) {
+    return <Error />;
+  }
+
+  const isOwned = () => {
+    if (user?.id === comments.user_id) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  return (
+    <section className="flex flex-col">
+      {comments.map(comment => (
+        <Card key={comment.id}>
+          <CardContent>
+            <CardDescription>{comment.comments}</CardDescription>
+          </CardContent>
+          {isOwned && <Button>삭제</Button>}
+        </Card>
+      ))}
+    </section>
+  );
+};
+
+export default CommentList;
