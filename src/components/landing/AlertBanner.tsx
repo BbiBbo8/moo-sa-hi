f6127ded26fcfb2aed46f5c27cd40ebf5ae2c66f
33hyun <lggx12345@gmail.com>
2025-04-15 01:17:04 +0900

    feat: 재난 알림 배너 컴포넌트에 API 호출 추가 및 데이터 표시 개선

--- src/components/landing/AlertBanner.tsx
+++ src/components/landing/AlertBanner.tsx
@@ -1,15 +1,34 @@
-import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert"
-import { AlertTriangle } from "lucide-react"
+"use client";
+
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { AlertTriangle } from "lucide-react";
+import { useQuery } from "@tanstack/react-query";
+import fetchDisasterAlert from "@/app/api/fetchDisasterAlert";
 
 const AlertBanner = () => {
+  const {
+    data: alerts,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["disaster-alerts"],
+    queryFn: fetchDisasterAlert,
+    refetchInterval: 1000 * 60, // 1분마다 새로고침
+  });
 
-    return (
-        <Alert variant="destructive" className="mx-20px" >
-            <AlertTriangle />
-            <AlertTitle>[지진] 서울 강서구 규모 4.0 지진 발생</AlertTitle>
-            <AlertDescription>부가 설명</AlertDescription>
-        </Alert>
-    )
-}
+  const alert = alerts?.[0];
+  if (isLoading || error || !alert) return null;
 
-export default AlertBanner
+  return (
+    <Alert variant="destructive" className="mx-20px">
+      <AlertTriangle className="mr-2 h-4 w-4" />
+      <AlertTitle>{alert.message}</AlertTitle>
+      <AlertDescription>
+        [ {alert.region}]
+         {alert.createdAt}
+      </AlertDescription>
+    </Alert>
+  );
+};
+
+export default AlertBanner;
