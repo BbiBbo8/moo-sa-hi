67a7b2b22c4b50a689a38d7c37ec59dc53389fc6
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-04-14 15:40:28 +0900

    feat: 대피소 리스트 지도 맵 마커 필터링 기능 만들기기

--- src/components/map/ShelterList.tsx
+++ src/components/map/ShelterList.tsx
@@ -1,19 +1,15 @@
-import { useShelters } from "@/hooks/shelter/useShelters";
+import { useMarkerStore } from "@/store/useMarkerStore";
 
 const ShelterList = () => {
-  const { data: shelters = [] } = useShelters();
-  return (
-    <div className="p-4">
-      {/* 대피소 개수 표시 */}
-      <p className="mb-3 text-sm text-gray-500">
-        내 주변 대피소 {shelters.length}
-      </p>
+  const checkShelters = useMarkerStore(state => state.checkShelters); // MarkerStore의 저장된 데이터 불러오기
 
+  return (
+    <div className="z-50 p-4">
       {/* 대피소 목록 렌더링 */}
-      {shelters.map(shelter => (
+      {checkShelters.map(shelter => (
         <div
           key={shelter.name + shelter.address}
-          className="mb-2 flex items-center justify-between p-3"
+          className="mb-2 flex items-center justify-between rounded-lg p-3 hover:bg-gray-200"
         >
           {/* 대피소 이름 및 주소 정보 */}
           <div className="flex flex-col gap-1">
--- src/hooks/shelter/useShelters.ts
+++ src/hooks/shelter/useShelters.ts
@@ -1,9 +1,8 @@
 import { useQuery } from "@tanstack/react-query";
 import fetchSheltersApi from "@/app/api/fetchSheltersApi";
-import { Shelter } from "@/types/shelter";
 
 export const useShelters = () =>
-  useQuery<Shelter[]>({
+  useQuery({
     queryKey: ["shelters"],
     queryFn: () => fetchSheltersApi(),
   });
