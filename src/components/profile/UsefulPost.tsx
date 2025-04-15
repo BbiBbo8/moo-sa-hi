6ec3a73e4bcdecb9d043084ed6443e290b858e0b
nahyun park <pnh13584@gmail.com>
2025-04-15 22:46:17 +0900

    comment: 주석 추가

--- src/components/profile/UsefulPost.tsx
+++ src/components/profile/UsefulPost.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const UsefulPost = () => {
   return (
+    // 프레임만 존재
     <section className="grid grid-cols-3 gap-2">
       <div className="bg-accent h-30 w-30" />
       <div className="bg-accent h-30 w-30" />
