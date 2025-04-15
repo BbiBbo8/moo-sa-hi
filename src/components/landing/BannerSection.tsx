0fb8b490ceee7886fcb87156b0ac75bc4982542b
33hyun <lggx12345@gmail.com>
2025-04-15 20:29:53 +0900

    fix: BannerSection 컴포넌트의 마진 수정

--- src/components/landing/BannerSection.tsx
+++ src/components/landing/BannerSection.tsx
@@ -2,7 +2,7 @@ import { Button } from "@/components/ui/button";
 
 const LandingBanner = () => {
   return (
-    <section className="relative w-full h-[300px] bg-blue-300 flex flex-col justify-center items-center text-center mx-20px">
+    <section className="relative h-[300px] bg-blue-300 flex flex-col justify-center items-center text-center mx-[20px]">
       <h1 className="text-3xl font-bold mb-2">빠르고 안전한 대피소 찾기</h1>
       <p>근처의 재난 대피소 정보를 한눈에 확인하세요</p>
 
