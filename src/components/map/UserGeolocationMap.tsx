8f1ebc05f684985fec63cc3c1d619f8e345a07e9
33hyun <lggx12345@gmail.com>
2025-04-15 19:23:49 +0900

    fix: location prop의 null 처리 추가

--- src/components/map/UserGeolocationMap.tsx
+++ src/components/map/UserGeolocationMap.tsx
@@ -4,18 +4,22 @@ const UserGeolocationMap = ({
   location,
   setCenter,
 }: {
-  location: { lat: number; lng: number };
+  location: { lat: number; lng: number } | null;
   setCenter: (center: { lat: number; lng: number }) => void;
 }) => {
+  if (!location) {
+    return;
+  }
+
   return (
     <Map
-      center={location} // 부모에서 전달 받은 center를 사용
-      style={{ width: "100%", height: "350px" }} // 지도 크기 설정
-      level={3} // 지도 확대 수준 (1: 최대 확대, 숫자가 클수록 축소)
+      center={location}
+      style={{ width: "100%", height: "350px" }}
+      level={3}
     >
       <MapMarker
         position={location}
-        onClick={() => setCenter(location)} //마커 클릭 시 부모의 center 업데이트
+        onClick={() => setCenter(location)}
       >
         <div>현재 위치</div>
       </MapMarker>
