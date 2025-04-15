3384823459021aed0f67e7b39cb446a59fda126e
nahyun park <pnh13584@gmail.com>
2025-04-16 02:16:45 +0900

    fix: 사용하지 않는 import 삭제

--- src/app/(pages)/profile/page.tsx
+++ src/app/(pages)/profile/page.tsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ProfileTabs from "@/components/profile/ProfileTabs";
 import ProfileCard from "@/components/profile/ProfileCard";
 
 const ProfilePage = () => {
   return (
-    <main className="m-auto my-2 p-2 text-center">
+    <main className="m-auto my-2 p-5">
       {/* 프로필 내용이 들어갈 위치 */}
       <ProfileCard />
     </main>
