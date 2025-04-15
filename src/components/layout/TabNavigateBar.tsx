91faa9eb62bce78c74969292baf600d3cfa55d92
nahyun park <pnh13584@gmail.com>
2025-04-16 00:45:52 +0900

    style: 탭 네비게이션 스타일 적용

--- src/components/layout/TabNavigateBar.tsx
+++ src/components/layout/TabNavigateBar.tsx
@@ -15,28 +15,30 @@ const TabNavigateBar = () => {
   }
 
   return (
-    <div className="bg-accent fixed bottom-0 z-50 inline-flex w-full items-center justify-evenly p-1 text-center">
-      <Link href={PATH.HOME}>
-        {/* 랜딩 페이지(홈)으로 이동 */}
-        <Home className="w-12 text-center" />
-        <h5 className="text-xs">홈</h5>
-      </Link>
-      <Link href={PATH.MAP}>
-        {/* 지도 페이지로 이동 */}
-        <MapPin className="ml-2 w-12 text-center" />
-        <h5 className="text-xs">대피소 지도</h5>
-      </Link>
-      <Link href={PATH.COMMUNITYSHELTER}>
-        {/* 커뮤니티 (기본은 대피소 커뮤니티)로 이동 */}
-        <FileText className="w-12 text-center" />
-        <h5 className="text-xs">커뮤니티</h5>
-      </Link>
-      <Link href={PATH.PROFILE}>
-        {/* 마이페이지로 이동 */}
-        <UserRound className="w-12 text-center" />
-        <h5 className="text-xs">마이</h5>
-      </Link>
-    </div>
+    <section className="flex w-full justify-center px-5">
+      <div className="fixed bottom-0 z-50 mb-4 inline-flex h-fit min-w-80 justify-evenly rounded-full border border-transparent bg-[#262626] text-center text-white">
+        <Link href={PATH.HOME} className="w-fit py-0.5">
+          {/* 랜딩 페이지(홈)으로 이동 */}
+          <Home className="h-7 w-full text-center" />
+          <h5 className="text-xs">홈</h5>
+        </Link>
+        <Link href={PATH.MAP} className="h-fit w-fit py-0.5">
+          {/* 지도 페이지로 이동 */}
+          <MapPin className="h-7 w-full text-center" />
+          <h5 className="text-xs">대피소</h5>
+        </Link>
+        <Link href={PATH.COMMUNITYSHELTER} className="w-fit py-0.5">
+          {/* 커뮤니티 (기본은 대피소 커뮤니티)로 이동 */}
+          <FileText className="h-7 w-full text-center" />
+          <h5 className="text-xs">커뮤니티</h5>
+        </Link>
+        <Link href={PATH.PROFILE} className="w-fit py-0.5">
+          {/* 마이페이지로 이동 */}
+          <UserRound className="h-7 w-full text-center" />
+          <h5 className="text-xs">마이</h5>
+        </Link>
+      </div>
+    </section>
   );
 };
 
