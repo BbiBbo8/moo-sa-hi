1c5205b97861fd3e967f559392912bad3000c708
nahyun park <pnh13584@gmail.com>
2025-04-15 09:53:08 +0900

    feat: 댓글 단 게시글 목록 생성

---
+++ src/components/profile/CommentPost.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const CommentPost = () => {
+  return (
+    <section className="grid grid-cols-3 gap-2">
+      <div className="bg-accent h-30 w-30" />
+      <div className="bg-accent h-30 w-30" />
+      <div className="bg-accent h-30 w-30" />
+      <div className="bg-accent h-30 w-30" />
+    </section>
+  );
+};
+
+export default CommentPost;
