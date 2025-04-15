018436fe2431a2b61ca044468fdeabdb6ccaffcc
nahyun park <pnh13584@gmail.com>
2025-04-15 19:01:28 +0900

    feat: 일상 게시글 불러오기 추가

--- src/supabase/getPosts.ts
+++ src/supabase/getPosts.ts
@@ -3,10 +3,18 @@ import createClient from "./client";
 const getPosts = async (id: string) => {
   try {
     const supabase = createClient();
-    const { data: shelter_post, error } = await supabase
+    // 유저 id 참조해 모든 커뮤니티 글 불러오기
+    const { data: shelter_post, error: shelterError } = await supabase
       .from("shelter_post")
-      .select("*");
-    return shelter_post;
+      .select("*")
+      .eq("user_id", id);
+
+    const { data: daily_post, error: dailyError } = await supabase
+      .from("daily_post")
+      .select("*")
+      .eq("user_id", id);
+
+    return { shelter_post, daily_post };
   } catch (error) {
     throw new Error("게시글 불러오기 실패");
   }
