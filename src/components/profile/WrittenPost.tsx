5402028d5dc81c54ed2041f80351594ae07bb463
nahyun park <pnh13584@gmail.com>
2025-04-15 09:50:47 +0900

    feat: 임시 프레임 생성

--- src/components/profile/WrittenPost.tsx
+++ src/components/profile/WrittenPost.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const WrittenPost = () => {
-  return <div>WrittenPost</div>;
+  return (
+    <section className="grid grid-cols-3 gap-2">
+      <div className="bg-accent h-30 w-30" />
+      <div className="bg-accent h-30 w-30" />
+      <div className="bg-accent h-30 w-30" />
+      <div className="bg-accent h-30 w-30" />
+    </section>
+  );
 };
 
 export default WrittenPost;
