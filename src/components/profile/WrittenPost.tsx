d655de06fbb0fe2908b84f4ea9e55d2e811dd8df
nahyun park <pnh13584@gmail.com>
2025-04-15 21:07:10 +0900

    feat: 이미지가 있으면 이미지로 채우기

--- src/components/profile/WrittenPost.tsx
+++ src/components/profile/WrittenPost.tsx
@@ -2,6 +2,7 @@
 
 import PATH from "@/constants/PATH";
 import useGetPosts from "@/hooks/useGetPosts";
+import Image from "next/image";
 import Link from "next/link";
 
 const WrittenPost = () => {
@@ -10,6 +11,14 @@ const WrittenPost = () => {
   const shelterPost = posts?.shelter_post;
   const dailyPost = posts?.daily_post;
 
+  const isImage = (postUrl: string | null) => {
+    if (postUrl === null) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
   return (
     <>
       <section className="grid grid-cols-3 gap-2 text-center">
@@ -17,9 +26,26 @@ const WrittenPost = () => {
         {shelterPost &&
           shelterPost?.map(post => (
             <Link href={PATH.COMMUNITYSHELTER + `/${post.id}`}>
-              <div key={post.id} className="bg-accent h-30 w-30">
-                <p>{post.title}</p>
-                <p>대피소</p>
+              <div
+                key={post.id}
+                className="bg-accent h-30 w-30 overflow-hidden bg-center"
+              >
+                {/* 이미지가 있으면 bg를 이미지로 변경 */}
+                {isImage(post.img_url) ? (
+                  <Image
+                    src={post.img_url}
+                    width={30}
+                    height={30}
+                    alt={post.title}
+                    className="h-30 w-30 object-cover"
+                  />
+                ) : null}
+                {!isImage(post.img_url) && (
+                  <>
+                    <p>{post.title}</p>
+                    <p></p>
+                  </>
+                )}
               </div>
             </Link>
           ))}
@@ -27,13 +53,31 @@ const WrittenPost = () => {
         {dailyPost &&
           dailyPost?.map(post => (
             <Link href={PATH.COMMUNITYDAILY + `/${post.id}`}>
-              <div key={post.id} className="bg-accent h-30 w-30">
-                <p>{post.title}</p>
-                <p>일상</p>
+              <div
+                key={post.id}
+                className="bg-accent h-30 w-30 overflow-hidden bg-center"
+              >
+                {/* 이미지가 있으면 bg를 이미지로 변경 */}
+                {isImage(post.img_url) ? (
+                  <Image
+                    src={post.img_url}
+                    width={30}
+                    height={30}
+                    alt={post.title}
+                    className="h-30 w-30 object-cover"
+                  />
+                ) : null}
+                {!isImage(post.img_url) && (
+                  <>
+                    <p>{post.title}</p>
+                    <p>일상</p>
+                  </>
+                )}
               </div>
             </Link>
           ))}
       </section>
+      {/* 작성 게시글이 없으면 보이기 */}
       {(!shelterPost || !dailyPost) && (
         <div>
           <p>지금 바로 게시글을 작성해보세요!</p>
