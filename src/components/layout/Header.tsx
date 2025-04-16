e63018dc1197a8c730f9892db58201fc8866508c
33hyun <lggx12345@gmail.com>
2025-04-16 14:33:35 +0900

    refactor: 공통헤더 상단 fixed

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -11,7 +11,11 @@ const Header = () => {
   // 현재 랜딩 페이지인지 확인하기
   const isLandingPage = pathname === PATH.HOME;
   // 헤더 숨길 페이지 목록
-  const hiddenRoutes:string[] = [PATH.CREATE,PATH.COMMUNITYSHELTER,PATH.COMMUNITYDAILY];
+  const hiddenRoutes: string[] = [
+    PATH.CREATE,
+    PATH.COMMUNITYSHELTER,
+    PATH.COMMUNITYDAILY,
+  ];
 
   // 특정 페이지에서 헤더 안 보이게
   if (hiddenRoutes.includes(pathname)) {
@@ -24,10 +28,10 @@ const Header = () => {
 
   const handleHome = () => {
     router.push(PATH.HOME);
-  }
+  };
 
   return (
-    <header className="flex w-full items-center justify-between border-b px-4 py-4">
+    <header className="fixed top-0 z-50 flex w-full items-center justify-between border-b bg-white px-4 py-4">
       {/* 뒤로가기 버튼 (랜딩 페이지에서는 숨김) */}
       {isLandingPage ? (
         <div className="h-5 w-5" /> // 공간만 유지
@@ -37,13 +41,20 @@ const Header = () => {
             src="/icons/chevron-left-solid 1.svg"
             alt="뒤로가기 아이콘"
             width={24}
-            height={24} />
+            height={24}
+          />
         </button>
       )}
 
       {/* 중앙 로고 자리 */}
-      <button onClick={handleHome}><Image src="/typos/logo.svg" alt="중앙로고" height={24} width={65}></Image></button>
-      
+      <button onClick={handleHome}>
+        <Image
+          src="/typos/logo.svg"
+          alt="중앙로고"
+          height={24}
+          width={65}
+        ></Image>
+      </button>
 
       {/* 오른쪽 여백 (뒤로가기와 균형 맞추기용) */}
       <div className="h-5 w-5" />
