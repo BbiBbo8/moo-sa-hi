9727946ee2be9b9c3b3513c7b15a9508b4c23afb
nahyun park <pnh13584@gmail.com>
2025-04-16 18:11:05 +0900

    feat: 닫기 아이콘 추가

--- src/components/profile/ProfileEditPop.tsx
+++ src/components/profile/ProfileEditPop.tsx
@@ -9,6 +9,7 @@ import { z } from "zod";
 import createClient from "@/supabase/client";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import LogoutButton from "../auth/LogoutButton";
+import Image from "next/image";
 
 interface ProfileEditPopProps {
   userId: string;
@@ -64,35 +65,43 @@ const ProfileEditPop = ({ userId }: ProfileEditPopProps) => {
         </Button>
       </PopoverTrigger>
       <div className="text-center">
-        <PopoverContent className="flex w-full flex-col rounded-lg p-4">
+        <PopoverContent className="flex w-fit flex-col rounded-lg p-4">
           <div className="grid gap-4">
+            <div className="w-full justify-end">
+              <Image
+                src="/icons/xmark-solid.svg"
+                alt="닫기"
+                width={28}
+                height={28}
+                objectFit="center"
+              />
+            </div>
             <div className="space-y-1">
               <h4 className="text-base font-semibold">닉네임 변경</h4>
             </div>
 
             <div className="grid gap-3">
               {/* 닉네임 입력 */}
-              <div className="grid grid-cols-3 items-center gap-4">
+              <div className="grid-row-3 grid items-center gap-4">
                 <Input
                   id="nickname"
                   value={editNickname}
                   onChange={e => setEditNickname(e.target.value)}
                   placeholder="닉네임 입력"
-                  className="col-span-2 h-fit w-full bg-[#F7F7F7] px-4 py-3 text-sm [active]:border-none"
+                  className="h-fit w-[280px] bg-[#F7F7F7] px-4 py-3 text-sm [active]:border-none"
                 />
-              </div>
-
-              {/* 저장 버튼 */}
-              <div className="mt-2 flex flex-col focus:outline-none">
-                <Button
-                  size="sm"
-                  onClick={() => mutate(editNickname)}
-                  disabled={isPending}
-                  className="w-full border border-[#CCCCCC] bg-transparent py-2 text-[#CCCCCC]"
-                >
-                  확인
-                </Button>
-                <LogoutButton />
+                {/* 저장 버튼 */}
+                <div className="mt-2 flex flex-col focus:outline-none">
+                  <Button
+                    size="sm"
+                    onClick={() => mutate(editNickname)}
+                    disabled={isPending}
+                    className="h-fit w-full border border-[#CCCCCC] bg-transparent py-3 text-sm text-[#CCCCCC]"
+                  >
+                    확인
+                  </Button>
+                  <LogoutButton />
+                </div>
               </div>
             </div>
           </div>
