664d4a0c77215575e59b27294f99529a8fb7e30d
nahyun park <pnh13584@gmail.com>
2025-04-16 20:46:21 +0900

    style: 팝오버 배경 스타일 생성

--- src/components/ui/PopOver.tsx
+++ src/components/ui/PopOver.tsx
@@ -18,18 +18,20 @@ const PopoverAnchor = PopoverPrimitive.Anchor;
 const PopoverContent = React.forwardRef<
   React.ComponentRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
+>(({ className, align = "center", sideOffset = 2, ...props }, ref) => (
   <PopoverPrimitive.Portal>
-    <PopoverPrimitive.Content
-      ref={ref}
-      align={align}
-      sideOffset={sideOffset}
-      className={cn(
-        "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 z-50 flex w-64 origin-[--radix-popover-content-transform-origin] flex-col rounded-md border border-transparent p-4 shadow-md outline-none",
-        className,
-      )}
-      {...props}
-    />
+    <div className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-40 bg-black/50">
+      <PopoverPrimitive.Content
+        ref={ref}
+        align={align}
+        sideOffset={0}
+        className={cn(
+          "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 z-50 flex w-full origin-[--radix-popover-content-transform-origin] -translate-x-1/7 rounded-md border border-transparent p-4 shadow-md outline-none",
+          className,
+        )}
+        {...props}
+      />
+    </div>
   </PopoverPrimitive.Portal>
 ));
 PopoverContent.displayName = PopoverPrimitive.Content.displayName;
