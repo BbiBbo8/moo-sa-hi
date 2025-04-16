87ed664c89993a09d9870da8e446d50057d0a43f
nahyun park <pnh13584@gmail.com>
2025-04-17 00:25:24 +0900

    fix: sideOffset 사용 오류 수정

--- src/components/ui/popover.tsx
+++ src/components/ui/popover.tsx
@@ -18,13 +18,13 @@ const PopoverAnchor = PopoverPrimitive.Anchor;
 const PopoverContent = React.forwardRef<
   React.ComponentRef<typeof PopoverPrimitive.Content>,
   React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 2, ...props }, ref) => (
+>(({ className, align = "center", sideOffset = 0, ...props }, ref) => (
   <PopoverPrimitive.Portal>
     <div className="data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 fixed inset-0 z-40 bg-black/50">
       <PopoverPrimitive.Content
         ref={ref}
         align={align}
-        sideOffset={0}
+        sideOffset={sideOffset}
         className={cn(
           "bg-popover text-popover-foreground data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 z-50 flex w-full origin-[--radix-popover-content-transform-origin] -translate-x-1/7 rounded-md border border-transparent p-4 shadow-md outline-none",
           className,
