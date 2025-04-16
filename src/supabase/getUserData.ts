c4635403ed70dcfe453bfbff2520f4faed9216a7
nahyun park <pnh13584@gmail.com>
2025-04-16 11:27:53 +0900

    fix: 함수 로직 수정

--- src/supabase/getUserData.ts
+++ src/supabase/getUserData.ts
@@ -9,13 +9,11 @@ const getUserData = () => {
       const {
         data: { user },
       } = await supabase.auth.getUser();
+      const userId = user?.id;
       const { data: userMetaData, error } = await supabase
         .from("users")
         .select("*")
-        .eq(
-          "id",
-          user?.id,
-        ) /* 가져온 유저를 참조해 users table 정보를 가져오기 */
+        .eq("id", userId) /* 가져온 유저를 참조해 users table 정보를 가져오기 */
         .single();
       return { user, userMetaData };
     } catch (error) {
