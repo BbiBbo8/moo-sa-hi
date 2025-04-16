3aa4eaade1f5f8f9921a6fae066f88cf448d9432
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-16 09:40:22 +0900

    style: 글 작성 플로팅 버튼 레이아웃 수정

--- src/components/community/PostCreateFloatingBtn.tsx
+++ src/components/community/PostCreateFloatingBtn.tsx
@@ -1,10 +1,17 @@
 import PATH from "@/constants/PATH";
+import Image from "next/image";
 import Link from "next/link";
 
 const PostCreateFloatingBtn = () => {
   return (
     <Link href={PATH.CREATE}>
-      <button className="flex h-[48px] w-[102px] items-center justify-center rounded-full border-1 border-gray-400 bg-white text-sm text-gray-500">
+      <button className="flex h-[48px] w-[102px] items-center justify-center gap-1 rounded-full border-1 border-gray-400 bg-white text-sm text-gray-500">
+        <Image
+          src={"/icons/PencilSimple.svg"}
+          alt={"icon"}
+          width={20}
+          height={20}
+        />
         글 작성
       </button>
     </Link>
