71bf5438fa271833406c579b49536547b16ff0b5
dlfhrrl12 <apfhdfhrrl@naver.com>
2025-04-21 14:59:33 +0900

    style: 목록 보기 버튼 수정

--- src/components/map/MapClient.tsx
+++ src/components/map/MapClient.tsx
@@ -9,11 +9,10 @@ import CrosshairButton from "./CrosshairButton";
 const MapClient = () => {
   return (
     <div className="relative flex h-screen w-screen flex-col items-center justify-center">
-      <div className="pointer-events-none relative flex flex-row justify-center">
-        <div className="flex justify-center">
-          <InputSearch />
-        </div>
+      <div className="flex justify-center">
+        <InputSearch />
       </div>
+
       <MainMap />
       <div className="absolute top-[80px] right-8 z-40 flex flex-col gap-3">
         <CrosshairButton />
--- src/components/map/ShelterDrawer.tsx
+++ src/components/map/ShelterDrawer.tsx
@@ -126,15 +126,25 @@ const ShelterDrawer = () => {
       {/* 목록 버튼 (중앙 하단) */}
       <button
         onClick={() => setIsOpen(true)}
-        className="fixed bottom-6 left-1/2 z-30 -translate-x-1/2"
+        className="fixed bottom-6 left-1/2 flex -translate-x-1/2 flex-row items-center gap-2 rounded-full bg-[#1A1A1A] px-5 py-2"
         aria-label="대피소 목록 열기"
       >
-        <Image
-          src="/icons/map/List-Open.svg"
-          alt="아이콘"
-          width={104}
-          height={40}
-        />
+        <svg
+          width="18"
+          height="20"
+          viewBox="0 0 18 20"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="bars-solid">
+            <path
+              id="Vector"
+              d="M0 3.75C0 3.05859 0.574554 2.5 1.28571 2.5H16.7143C17.4254 2.5 18 3.05859 18 3.75C18 4.44141 17.4254 5 16.7143 5H1.28571C0.574554 5 0 4.44141 0 3.75ZM0 10C0 9.30859 0.574554 8.75 1.28571 8.75H16.7143C17.4254 8.75 18 9.30859 18 10C18 10.6914 17.4254 11.25 16.7143 11.25H1.28571C0.574554 11.25 0 10.6914 0 10ZM18 16.25C18 16.9414 17.4254 17.5 16.7143 17.5H1.28571C0.574554 17.5 0 16.9414 0 16.25C0 15.5586 0.574554 15 1.28571 15H16.7143C17.4254 15 18 15.5586 18 16.25Z"
+              fill="white"
+            />
+          </g>
+        </svg>
+        <p className="text-base font-medium text-white">목록보기</p>
       </button>
 
       {/* Drawer */}
