f3d65a23672e54fc9a5b6c0a8750437600583704
nahyun park <pnh13584@gmail.com>
2025-04-22 23:38:27 +0900

    feat: 댓글 구분선 추가

--- src/components/community/CommentList.tsx
+++ src/components/community/CommentList.tsx
@@ -20,7 +20,7 @@ const CommentList = ({ postId }: { postId: number }) => {
     isLoading: isCommentLoading,
   } = useComments({ postId });
 
-  //   댓글 작성자 여부 확인
+  // 댓글 작성자 여부 확인
   const isOwned = (commentUserId: string | null) => {
     if (user?.id === commentUserId) {
       return true;
@@ -29,7 +29,7 @@ const CommentList = ({ postId }: { postId: number }) => {
     }
   };
 
-  //   댓글 삭제 함수 호출
+  // 댓글 삭제 함수 호출
   const deleteCommentMutation = useDeleteComment();
   // 로딩 중일 때 로딩중 컴포넌트 표시
   if (isLoading || isCommentLoading) {
@@ -41,28 +41,31 @@ const CommentList = ({ postId }: { postId: number }) => {
   }
 
   return (
-    <section className="m-4 mb-12 flex flex-col gap-4">
-      {comments?.map(comment => (
-        <Card key={comment.id} className="border-block shadow-none">
-          <CardContent>
-            <CardDescription className="text-[#1A1A1A]">
-              {comment.comments}
-            </CardDescription>
-            <div className="mt-3 flex flex-row">
-              <CardDescription className="py-2 text-[#B3B3B3]">
-                {elapsedTime(comment.created_at)}
+    <section className="mb-12 flex flex-col">
+      {comments?.map((comment, index) => (
+        <div key={comment.id}>
+          {index > 0 && <div className="h-0.25 w-full bg-[#F2F2F2]" />}
+          <Card className="border-none shadow-none">
+            <CardContent>
+              <CardDescription className="text-[#1A1A1A]">
+                {comment.comments}
               </CardDescription>
-              {isOwned(comment.user_id) && (
-                <Button
-                  onClick={() => deleteCommentMutation.mutate(comment.id)}
-                  className="border-block h-fit w-fit bg-transparent font-normal text-[#B3B3B3]"
-                >
-                  댓글 삭제
-                </Button>
-              )}
-            </div>
-          </CardContent>
-        </Card>
+              <div className="mt-3 flex flex-row">
+                <CardDescription className="py-2 text-[#B3B3B3]">
+                  {elapsedTime(comment.created_at)}
+                </CardDescription>
+                {isOwned(comment.user_id) && (
+                  <Button
+                    onClick={() => deleteCommentMutation.mutate(comment.id)}
+                    className="border-block h-fit w-fit bg-transparent font-normal text-[#B3B3B3]"
+                  >
+                    댓글 삭제
+                  </Button>
+                )}
+              </div>
+            </CardContent>
+          </Card>
+        </div>
       ))}
     </section>
   );
