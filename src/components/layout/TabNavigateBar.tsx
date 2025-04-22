5b554ee3c8f8bce6724841a6ec2e3f73fbd8359d
nahyun park <pnh13584@gmail.com>
2025-04-22 21:02:19 +0900

    style: 아이콘 스타일 적용

--- src/components/layout/TabNavigateBar.tsx
+++ src/components/layout/TabNavigateBar.tsx
@@ -19,54 +19,96 @@ const TabNavigateBar = () => {
   if (shouldHide) return null;
 
   return (
-    <section className="flex w-full justify-center">
-      <div className="fixed bottom-0 z-50 mb-4 inline-flex h-fit w-11/12 justify-evenly rounded-full border border-transparent bg-[#262626] px-5 text-center text-sm font-medium text-white">
-        <Link href={PATH.HOME} className="h-fit w-fit py-1">
+    <section className="fixed bottom-0 z-50 flex h-fit w-full justify-center border border-[#FAFAFA] bg-[#FAFAFA]">
+      <div className="flex w-full justify-between bg-[#FAFAFA] px-5 py-1.5 text-center text-sm font-medium">
+        <Link href={PATH.HOME} className="h-full min-w-1/5">
           <div className="flex flex-col items-center">
             {/* 랜딩 페이지(홈)으로 이동 */}
             <Image
-              src="/icons/house-solid.svg"
+              src={
+                pathname === PATH.HOME
+                  ? "/icons/layout/house-solid-blue.svg"
+                  : "/icons/layout/house-solid-grey.svg"
+              }
               alt="홈"
               width={28}
               height={28}
             />
-            <h5>홈</h5>
+            <h5
+              className={
+                pathname === PATH.HOME ? "text-[#2889E4]" : "text-[#B3B3B3]"
+              }
+            >
+              홈
+            </h5>
           </div>
         </Link>
-        <Link href={PATH.MAP} className="h-fit w-fit py-1">
+        <Link href={PATH.MAP} className="h-full min-w-1/5">
           <div className="flex flex-col items-center">
             {/* 지도 페이지로 이동 */}
             <Image
-              src="/icons/map-location-dot-solid.svg"
+              src={
+                pathname === PATH.MAP
+                  ? "/icons/layout/map-location-dot-solid-blue.svg"
+                  : "/icons/layout/map-location-dot-solid-grey.svg"
+              }
               alt="지도"
               width={28}
               height={28}
             />
-            <h5>대피소</h5>
+            <h5
+              className={
+                pathname === PATH.MAP ? "text-[#2889E4]" : "text-[#B3B3B3]"
+              }
+            >
+              대피소
+            </h5>
           </div>
         </Link>
-        <Link href={PATH.COMMUNITYSHELTER} className="w-fit py-1">
+        <Link href={PATH.COMMUNITYSHELTER} className="h-full min-w-1/5">
           <div className="flex flex-col items-center">
             {/* 커뮤니티 (기본은 대피소 커뮤니티)로 이동 */}
             <Image
-              src="/icons/comments-solid-1.svg"
+              src={
+                pathname.startsWith(PATH.COMMUNITYSHELTER)
+                  ? "/icons/layout/comments-solid-blue.svg"
+                  : "/icons/layout/comments-solid-grey.svg"
+              }
               alt="커뮤니티"
               width={28}
               height={28}
             />
+            <h5
+              className={
+                pathname.startsWith(PATH.COMMUNITYSHELTER)
+                  ? "pt-0.5 text-[#2889E4]"
+                  : "pt-0.5 text-[#B3B3B3]"
+              }
+            >
+              커뮤니티
+            </h5>
           </div>
-          <h5>커뮤니티</h5>
         </Link>
-        <Link href={PATH.PROFILE} className="w-fit py-1">
-          <div className="flex flex-col items-center">
+        <Link href={PATH.PROFILE} className="h-full min-w-1/5">
+          <div className="flex flex-col items-center justify-center">
             {/* 마이페이지로 이동 */}
             <Image
-              src="/icons/user-solid.svg"
+              src={
+                pathname === PATH.PROFILE
+                  ? "/icons/layout/user-solid-blue.svg"
+                  : "/icons/layout/user-solid-grey.svg"
+              }
               alt="마이"
-              width={28}
-              height={28}
+              width={24}
+              height={24}
             />
-            <h5>마이</h5>
+            <h5
+              className={
+                pathname === PATH.PROFILE ? "text-[#2889E4]" : "text-[#B3B3B3]"
+              }
+            >
+              마이
+            </h5>
           </div>
         </Link>
       </div>
