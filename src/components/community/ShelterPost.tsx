bd1168dce40c541cffae59c532e67e6ab5cbe826
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-23 06:05:50 +0900

    style: 대피소 포스트 카드 레이아웃 수정

--- src/components/community/ShelterPost.tsx
+++ src/components/community/ShelterPost.tsx
@@ -33,9 +33,9 @@ const ShelterPost = ({ post }: PostCardProps) => {
     >
       <Card
         key={post.id}
-        className="h-[142px] w-full gap-3 rounded-none border-b-1 border-[#F2F2F2] px-5 shadow-none"
+        className="h-[142px] w-full gap-3 rounded-none border-b-1 border-[#F2F2F2] px-5 py-4 shadow-none"
       >
-        <CardContent className="h-full p-0">
+        <CardContent className="px-0">
           <section className="flex h-full w-full flex-col items-start justify-center gap-2">
             <CardTitle
               className={`flex items-center justify-center rounded-[8px] px-2 py-1 text-sm text-[14px] font-medium text-[#1A1A1A] text-white ${colorForPopulation}`}
@@ -43,22 +43,24 @@ const ShelterPost = ({ post }: PostCardProps) => {
               {populationDensity}
             </CardTitle>
             <article className="flex w-[321px] flex-col gap-0">
-              <h2 className="truncate text-[16px] font-semibold">
+              <h2 className="text-titleM truncate text-gray-800">
                 {post.title}
               </h2>
-              <span className="truncate text-sm">{post.shelter_name}</span>
+              <span className="text-bodyL truncate text-gray-500">
+                {post.shelter_name}
+              </span>
             </article>
 
-            <section className="flex w-full flex-row justify-between text-sm/[135%] text-[#B3B3B3]">
+            <section className="text-numEng flex w-full flex-row justify-between text-gray-300">
               <p>{timeCreated}</p>
-              <div className="flex items-center gap-1">
+              <div className="text-numEng flex items-center gap-1 text-gray-300">
                 <Image
-                  src={"/icons/thumbs-up-solid.svg"}
+                  src={"/icons/community/thumbs-up-gray.svg"}
                   alt={"icon"}
                   width={20}
                   height={20}
                 />
-                <span className="text-sm">{post.helpfuls?.length ?? 0}</span>
+                <span>{post.helpfuls?.length ?? 0}</span>
               </div>
             </section>
           </section>
