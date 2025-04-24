8f542ff64034863bd5c226dd48e3a17b24dc5469
nahyun park <pnh13584@gmail.com>
2025-04-24 15:13:24 +0900

    feat: 유용해요 함수 생성

--- src/components/profile/HelpfulPost.tsx
+++ src/components/profile/HelpfulPost.tsx
@@ -1,13 +1,74 @@
-import React from "react";
+"use client";
+
+import Error from "@/app/(pages)/Error";
+import Loading from "@/app/(pages)/Loading";
+import PATH from "@/constants/PATH";
+import { useGetHelpfuls } from "@/hooks/profile/useGetHelpfuls";
+import Image from "next/image";
+import Link from "next/link";
 
 const HelpfulPost = () => {
+  const { data: helpfuls, error, isLoading } = useGetHelpfuls();
+  if (isLoading) {
+    return <Loading />;
+  }
+  if (error) {
+    return <Error />;
+  }
+
+  // 이미지 존재 여부 판단하는 함수
+  const isImage = (postUrl: string | null) => {
+    if (postUrl === "" || postUrl === null) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   return (
     // 프레임만 존재
     <section className="grid grid-cols-3 gap-0.5 text-center">
-      <div className="bg-accent grid min-h-32 min-w-32 truncate overflow-hidden bg-center" />
-      <div className="bg-accent grid min-h-32 min-w-32 truncate overflow-hidden bg-center" />
-      <div className="bg-accent grid min-h-32 min-w-32 truncate overflow-hidden bg-center" />
-      <div className="bg-accent grid min-h-32 min-w-32 truncate overflow-hidden bg-center" />
+      {helpfuls?.map(helpful => (
+        <div
+          key={helpful.id}
+          className="bg-accent relative grid aspect-square min-h-32 min-w-32 truncate overflow-hidden bg-center"
+        >
+          {helpful.daily_post === null && (
+            <Link href={`${PATH.COMMUNITYSHELTER}/${helpful.id}`}>
+              <div className="min-h-32 min-w-32">
+                {/* 이미지가 존재할 때 썸네일 띄우기 */}
+                {isImage(helpful.shelter_post?.img_url!) ? (
+                  <Image
+                    src={helpful.shelter_post?.img_url as string}
+                    alt="이미지"
+                    fill
+                    objectFit="cover"
+                  />
+                ) : (
+                  <p className="mt-[42%]">{helpful.shelter_post?.title}</p>
+                )}
+              </div>
+            </Link>
+          )}
+          {helpful.shelter_post === null && (
+            <Link href={`${PATH.COMMUNITYDAILY}/${helpful.id}`}>
+              <div className="min-h-32 min-w-32">
+                {/* 이미지가 존재할 때 썸네일 띄우기 */}
+                {isImage(helpful.daily_post?.img_url!) ? (
+                  <Image
+                    src={helpful.daily_post?.img_url as string}
+                    alt="이미지"
+                    fill
+                    objectFit="cover"
+                  />
+                ) : (
+                  <p className="mt-[42%]">{helpful.daily_post?.title}</p>
+                )}
+              </div>
+            </Link>
+          )}
+        </div>
+      ))}
     </section>
   );
 };
