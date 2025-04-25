0932ba3a7de30dce988a72718aded30d28962936
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-25 14:31:58 +0900

    style: 랜딩 페이지 레이아웃 이슈 수정

--- src/components/landing/AlertBanner.tsx
+++ src/components/landing/AlertBanner.tsx
@@ -22,40 +22,38 @@ const AlertBanner = () => {
   if (isLoading || error || !alert) return null;
 
   return (
-    <div className="px-[20px]">
-      <div className="flex w-full items-start justify-between rounded-lg bg-white px-4 py-3 shadow">
-        {/* 왼쪽: 아이콘 + 메시지 */}
-        <div className="flex items-start gap-2 pr-2">
-          <Image
-            src="/icons/bullhorn-solid1.svg"
-            alt="경고 아이콘"
-            width={18}
-            height={18}
-            className="mt-1"
-          />
-          <p
-            className={`text-[16px] text-gray-800 ${
-              isOpen ? "whitespace-pre-wrap" : "max-w-[250px] truncate"
-            }`}
-          >
-            [ {alert.region}] &nbsp;{alert.message}
-          </p>
-        </div>
-
-        {/* 오른쪽: 드롭다운 버튼 */}
-        <button onClick={() => setIsOpen(prev => !prev)} className="shrink-0">
-          <Image
-            src={
-              isOpen
-                ? "/icons/chevron-up-solid.svg"
-                : "/icons/chevron-down-solid.svg"
-            }
-            alt="드롭다운"
-            width={20}
-            height={20}
-          />
-        </button>
+    <div className="bg-gray-20 flex w-full items-start justify-between px-5 py-3">
+      {/* 왼쪽: 아이콘 + 메시지 */}
+      <div className="flex items-start gap-2 pr-2">
+        <Image
+          src="/icons/bullhorn-solid1.svg"
+          alt="경고 아이콘"
+          width={18}
+          height={18}
+          className="mt-1"
+        />
+        <p
+          className={`text-[16px] text-gray-800 ${
+            isOpen ? "whitespace-pre-wrap" : "max-w-[250px] truncate"
+          }`}
+        >
+          [ {alert.region}] &nbsp;{alert.message}
+        </p>
       </div>
+
+      {/* 오른쪽: 드롭다운 버튼 */}
+      <button onClick={() => setIsOpen(prev => !prev)} className="shrink-0">
+        <Image
+          src={
+            isOpen
+              ? "/icons/chevron-up-solid.svg"
+              : "/icons/chevron-down-solid.svg"
+          }
+          alt="드롭다운"
+          width={20}
+          height={20}
+        />
+      </button>
     </div>
   );
 };
