27e57ac5a8b73d6630b25347fdb4af5ef84c47f2
33hyun <lggx12345@gmail.com>
2025-04-28 14:39:04 +0900

    style: 닉네임 페이지의 패딩 수정 및 로고 SVG 추가

--- src/app/(pages)/auth/nickname/page.tsx
+++ src/app/(pages)/auth/nickname/page.tsx
@@ -18,7 +18,7 @@ const NicknamePage = async () => {
   }
 
   return (
-    <div className="p-6">
+    <div className="p-6 pt-20">
       <h1 className="mb-4 text-2xl font-semibold">닉네임 등록</h1>
       <NicknameForm userId={user.id} />
     </div>
