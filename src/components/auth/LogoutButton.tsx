ca8279efa899a5067d9f096ef1265834031e3aed
33hyun <lggx12345@gmail.com>
2025-04-28 17:34:34 +0900

    style: 로그아웃 버튼에서 캐시 무효화 기능 추가

--- src/components/auth/LogoutButton.tsx
+++ src/components/auth/LogoutButton.tsx
@@ -5,6 +5,7 @@ import { Button } from "../ui/button";
 import createClient from "@/supabase/client";
 import PATH from "@/constants/PATH";
 import { toast } from "sonner";
+import { revalidatePath } from "next/cache";
 
 const LogoutButton = () => {
   const router = useRouter();
@@ -14,11 +15,12 @@ const LogoutButton = () => {
     const { error } = await supabase.auth.signOut();
     if (error) {
       console.error("로그아웃 실패:", error.message);
+      toast.error("로그아웃 실패");
       return;
     }
     toast.success("로그아웃 성공");
-    router.push(PATH.HOME); // 로그아웃 후에 랜딩페이지로 이동
-    location.reload(); // 페이지 새로고침
+    router.push(PATH.HOME); // 로그아웃 후에 랜딩 페이지로 이동
+    revalidatePath(PATH.HOME); // 랜딩 페이지 데이터 다시 가져오기 (캐시 무효화)
   };
   return (
     <Button
