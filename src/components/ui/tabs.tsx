d181fb8b1b7b2d5a5e7b5185bc3d861a2b963fe2
nahyun park <pnh13584@gmail.com>
2025-04-28 11:40:58 +0900

    style: 탭 justify 조정

--- src/components/ui/tabs.tsx
+++ src/components/ui/tabs.tsx
@@ -15,7 +15,7 @@ const TabsList = React.forwardRef<
   <TabsPrimitive.List
     ref={ref}
     className={cn(
-      "inline-flex h-fit w-full items-center justify-between p-1 text-[#999999] md:px-18",
+      "inline-flex h-fit w-full items-center justify-evenly p-1 text-[#999999] md:justify-between md:px-16",
       className,
     )}
     {...props}
