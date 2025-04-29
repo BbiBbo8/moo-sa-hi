a0b8dd2e8f86000d73396b44f46d4c46ac637737
Sumin-Lee12 <leesumin041@gmail.com>
2025-04-30 00:46:15 +0900

    fix: 커뮤니티 페이지 배너가 보이지 않는 이슈 해결

--- src/components/community/detail/banner.tsx
+++ src/components/community/detail/banner.tsx
@@ -11,14 +11,14 @@ const CommunityBanner = () => {
   return (
     <>
       {isBannerVisible && (
-        <div className="top-16 left-5 flex w-full flex-row items-center justify-between gap-2 bg-[#F7F7F7] px-5 py-2">
+        <div className="flex w-full flex-row items-center justify-between gap-2 bg-[#F7F7F7] px-5 py-2">
           <div className="flex flex-row items-center justify-center gap-1 text-sm text-gray-600">
-            <Image src={bullhorn} alt={"icon"} width={24} height={24} />
+            <Image src={bullhorn} alt="icon" width={24} height={24} />
             <span>대피소 관련 경험과 정보를 솔직하게 공유해주세요.</span>
           </div>
           <Image
             src={xmark}
-            alt={"icon"}
+            alt="icon"
             width={20}
             height={20}
             onClick={() => setIsBannerVisible(false)}
